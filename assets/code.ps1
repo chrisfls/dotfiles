4df1b70a52cac1787cfcbc102e82c635b5c24d12
Chris <2013206+kress95@users.noreply.github.com>
2023-03-19 14:48:14 -0300

    remove legacy vscode shit

--- assets/code.ps1
+++
@@ -1,4 +0,0 @@
-# copy this to the same folder as code.cmd on windows
-$Env:VSCODE_DEV = $null
-$Env:ELECTRON_RUN_AS_NODE = 1
-& "$PSScriptRoot\..\Code.exe" "$PSScriptRoot\..\resources\app\out\cli.js" --ms-enable-electron-run-as-node $args
