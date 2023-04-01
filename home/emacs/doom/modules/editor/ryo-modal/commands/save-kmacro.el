52b61a0a4d3ba4d4cca7fc6b01595c499419899d
Chris <2013206+kress95@users.noreply.github.com>
2023-04-01 15:00:34 -0300

    before changing back to stock

--- home/emacs/doom/modules/editor/ryo-modal/commands/save-kmacro.el
+++ home/emacs/doom/modules/editor/ryo-modal/commands/save-kmacro.el
@@ -1,21 +1,6 @@
-;; TODO: if possible stop terminating macros on any warning
+(use-package! ryo-modal)
 
 (defvar saved-kmacro nil)
-
-(defun start-or-save-kmacro ()
-  (interactive)
-  (if defining-kbd-macro
-      (progn
-        (end-kbd-macro)
-        (setq saved-kmacro last-kbd-macro))
-    (start-kbd-macro nil)))
-
-(defun execute-saved-kmacro ()
-  (interactive)
-  (if (null saved-kmacro)
-      (message "No saved macro.")
-    (ignore-errors (execute-kbd-macro saved-kmacro))
-    (message "Executed saved macro.")))
 
 (defun start-or-cancel-kmacro ()
   (interactive)
@@ -23,7 +8,30 @@
       (setq defining-kbd-macro nil)
     (start-kbd-macro nil)))
 
-    ;; (let ((debug-on-error nil))
-    ;;   (start-kbd-macro nil))))
+(defun resume-or-save-kmacro ()
+  (interactive)
+  (if defining-kbd-macro
+      (progn
+        (end-kbd-macro)
+        (setq saved-kmacro last-kbd-macro))
+    (setq defining-kbd-macro t)))
+
+(defun execute-saved-kmacro ()
+  (interactive)
+  (if (null saved-kmacro)
+      (message "No saved macro.")
+    (let ((saved-prefix last-prefix-arg))
+      (ignore-errors (execute-kbd-macro saved-kmacro))
+      (setq ryo-modal--last-command-prefix-arg saved-prefix)
+      (setq ryo-modal--last-command #'execute-saved-kmacro)
+      (message "Executed saved macro."))))
+
+(defun execute-kmacro ()
+  (interactive)
+  (let ((saved-prefix last-prefix-arg))
+    (ignore-errors (kmacro-call-macro))
+    (setq ryo-modal--last-command-prefix-arg saved-prefix)
+    (setq ryo-modal--last-command #'execute-kmacro)
+    (message "Executed saved macro.")))
 
 (provide 'save-kmacro)
