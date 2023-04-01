298af5afeed5d2910ec1752380b509e63dd367c6
Chris <2013206+kress95@users.noreply.github.com>
2023-04-01 18:49:05 -0300

    fix kmacro issues

--- home/emacs/doom/modules/editor/ryo-modal/commands/save-kmacro.el
+++ home/emacs/doom/modules/editor/ryo-modal/commands/save-kmacro.el
@@ -1,37 +1,35 @@
 (use-package! ryo-modal)
 
-(defvar saved-kmacro nil)
-
-(defun start-or-cancel-kmacro ()
+(defun toggle-recording-macro ()
   (interactive)
   (if defining-kbd-macro
-      (setq defining-kbd-macro nil)
-    (start-kbd-macro nil)))
+      (progn 
+        (end-kbd-macro)
+        (message "Paused macro recording."))
+    (start-kbd-macro nil)
+    (if last-kbd-macro
+        (message "Resumed macro recording.")
+        (message "Started macro recording."))))
 
-(defun resume-or-save-kmacro ()
+(defun start-or-save-macro ()
   (interactive)
   (if defining-kbd-macro
       (progn
         (end-kbd-macro)
-        (setq saved-kmacro last-kbd-macro))
-    (setq defining-kbd-macro t)))
+        (kmacro-push-ring)
+        (message "Ended macro recording."))
+    (setq last-kbd-macro nil)
+    (start-kbd-macro nil)
+    (message "Started macro recording.")))
 
-(defun execute-saved-kmacro ()
-  (interactive)
-  (if (null saved-kmacro)
-      (message "No saved macro.")
-    (let ((saved-prefix last-prefix-arg))
-      (ignore-errors (execute-kbd-macro saved-kmacro))
-      (setq ryo-modal--last-command-prefix-arg saved-prefix)
-      (setq ryo-modal--last-command #'execute-saved-kmacro)
-      (message "Executed saved macro."))))
-
-(defun execute-kmacro ()
+(defun execute-macro ()
   (interactive)
   (let ((saved-prefix last-prefix-arg))
-    (ignore-errors (kmacro-call-macro))
+    (if (null last-kbd-macro)
+        (error "No recorded macro.")
+      (ignore-errors (execute-kbd-macro last-kbd-macro))
+      (message "Executed macro."))
     (setq ryo-modal--last-command-prefix-arg saved-prefix)
-    (setq ryo-modal--last-command #'execute-kmacro)
-    (message "Executed saved macro.")))
+    (setq ryo-modal--last-command #'execute-macro)))
 
 (provide 'save-kmacro)
