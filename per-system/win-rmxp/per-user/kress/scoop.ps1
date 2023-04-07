de8ec6659671d6c3c080bbcb35b26d41f2f3ea04
Chris <2013206+kress95@users.noreply.github.com>
2023-04-07 20:24:05 -0300

    scoop and winget

---
+++ per-system/win-rmxp/per-user/kress/scoop.ps1
@@ -0,0 +1,12 @@
+scoop install aria2 7zip git
+scoop config aria2-warning-enabled false
+scoop install 
+scoop bucket add extras
+scoop bucket add nerd-fonts
+scoop bucket add nonportable
+scoop bucket add versions
+scoop bucket add kress95 https://github.com/kress95/scoop-bucket
+scoop update
+scoop install adb anydesk audacity dark Delugia-Nerd-Font-Complete deno discord emacs fd ffmpeg gallery-dl git github glazewm godot hide-volume-osd innounp JetBrains-Mono JetBrainsMono-NF-Mono krita llvm lmms micro mkvtoolnix moonlight notepadplusplus npiperelay obsidian picotorrent powertoys pwsh ripgrep scoop-search sublime-text sudo thorium-avx2 ungoogled-chromium vcredist2022 vlc volume2 vscode winaero-tweaker windows-terminal yt-dlp
+sudo scoop install --global asio4all-np equalizer-apo-np hide-volume-osd-np parsec-np steam
+echo "Now install other stuff from steam like aseprite"
---
+++ per-system/win-rmxp/per-user/kress/winget.ps1
@@ -0,0 +1,4 @@
+winget install -e --id Logitech.GHUB
+winget install -e --id Logitech.OptionsPlus
+winget install -e --id Logitech.LogiBolt
+echo "Now install 'https://vb-audio.com/Cable/#DownloadASIOBridge'"
