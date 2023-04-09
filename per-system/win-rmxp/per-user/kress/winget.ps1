9de95641da57aad41b6c6736c1629e75e27d8a4e
Chris <2013206+kress95@users.noreply.github.com>
2023-04-09 19:42:36 -0300

    bump

--- per-system/win-rmxp/per-user/kress/winget.ps1
+++ per-system/win-rmxp/per-user/kress/winget.ps1
@@ -1,4 +1,5 @@
 winget install -e --id Logitech.GHUB
 winget install -e --id Logitech.OptionsPlus
 winget install -e --id Logitech.LogiBolt
+winget install -e --id Microsoft.PowerShell
 echo "Now install 'https://vb-audio.com/Cable/#DownloadASIOBridge'"
