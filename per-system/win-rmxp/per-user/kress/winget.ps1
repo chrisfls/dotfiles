cecb16c9284ff5314ec2e24864b1ff7a44a89659
Chris <2013206+kress95@users.noreply.github.com>
2023-04-10 02:45:35 -0300

    winget discord

--- per-system/win-rmxp/per-user/kress/winget.ps1
+++ per-system/win-rmxp/per-user/kress/winget.ps1
@@ -2,4 +2,5 @@ winget install -e --id Logitech.GHUB
 winget install -e --id Logitech.OptionsPlus
 winget install -e --id Logitech.LogiBolt
 winget install -e --id Microsoft.PowerShell
+winget install -e --id Discord.Discord
 echo "Now install 'https://vb-audio.com/Cable/#DownloadASIOBridge'"
