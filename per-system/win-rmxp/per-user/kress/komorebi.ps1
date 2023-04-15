f2da9b65e02401ac0279e1bf93069f6f6cac8a80
Chris <2013206+kress95@users.noreply.github.com>
2023-04-15 03:55:55 -0300

    komorebi var warning

--- per-system/win-rmxp/per-user/kress/komorebi.ps1
+++ per-system/win-rmxp/per-user/kress/komorebi.ps1
@@ -6,3 +6,4 @@ $Link.IconLocation = "$(scoop prefix komorebi)\komorebic.exe"
 $Link.WorkingDirectory = "$(scoop prefix komorebi)"
 $Link.Save()
 Copy-Item "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\komorebi.lnk" "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\komorebi.lnk"
+Write-Host "Don't forget to setup KOMOREBI_CONFIG_HOME"
