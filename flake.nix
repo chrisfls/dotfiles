22eaeecfac164d57eb7638f570b5d950422e3a6d
Chris <2013206+kress95@users.noreply.github.com>
2023-05-28 01:25:02 -0300

    downgrade home-manager

--- flake.nix
+++ flake.nix
@@ -9,13 +9,13 @@
   };
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
     nixos-wsl = {
       url = "github:nix-community/NixOS-WSL";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager/master";
+      url = "github:nix-community/home-manager/release-22.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     agenix = {
