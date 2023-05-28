c8e781a5a9b595d3fb4e59ffc3256f5ea5762ff9
Chris <2013206+kress95@users.noreply.github.com>
2023-05-28 01:10:54 -0300

    stable home manager

--- scripts/install-arch-wsl-rmxp.sh
+++ scripts/install-arch-wsl-rmxp.sh
@@ -99,7 +99,7 @@ else
   # install home manager
   nix-channel --add https://nixos.org/channels/nixos-22.11 nixpkgs
   nix-channel --add https://nixos.org/channels/nixpkgs-unstable nixpkgs-unstable
-  nix-channel --add "https://github.com/nix-community/home-manager/archive/master.tar.gz" home-manager
+  nix-channel --add https://github.com/nix-community/home-manager/archive/release-22.11.tar.gz home-manager
   nix-channel --update
   nix-shell '<home-manager>' -A install
 
