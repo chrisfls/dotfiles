dc2108f0b52132454103a65f7b25b6eaf4d4666e
Chris <2013206+kress95@users.noreply.github.com>
2023-06-23 01:41:25 -0300

    fix wt

--- per-system/win-rmxp/per-user/kress/wt.ps1
+++ per-system/win-rmxp/per-user/kress/wt.ps1
@@ -1,4 +1,4 @@
-$target = "$(scoop prefix windows-terminal)\settings\settings.json"
+$target = "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
 New-Item -ItemType SymbolicLink `
 -Path $target `
 -Target (Resolve-Path -Path "..\..\..\..\assets\Windows Terminal\settings.json")
