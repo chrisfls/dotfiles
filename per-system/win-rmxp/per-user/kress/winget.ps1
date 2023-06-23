301fdb50f97ff0e433cc2f4be93ab1cf2edbc38c
Chris <2013206+kress95@users.noreply.github.com>
2023-06-23 01:52:51 -0300

    sort

--- per-system/win-rmxp/per-user/kress/winget.ps1
+++ per-system/win-rmxp/per-user/kress/winget.ps1
@@ -1,33 +1,33 @@
 winget install -e --id AnyDeskSoftwareGmbH.AnyDesk
 winget install -e --id Audacity.Audacity
 winget install -e --id Discord.Discord
+winget install -e --id eloston.ungoogled-chromium
 winget install -e --id GIMP.GIMP
 winget install -e --id GNU.Emacs
 winget install -e --id irzyxa.Volume2Portable
 winget install -e --id ItchIo.Itch
 winget install -e --id KDE.Krita
+winget install -e --id Lapce.Lapce
 winget install -e --id lars-berger.GlazeWM
 winget install -e --id LMMS.LMMS
 winget install -e --id Logitech.GHUB
 winget install -e --id Logitech.LogiBolt
 winget install -e --id Logitech.OptionsPlus
 winget install -e --id marha.VcXsrv
+winget install -e --id MichaelTippach.ASIO4ALL
 winget install -e --id Microsoft.PowerShell
+winget install -e --id Microsoft.VisualStudioCode
+winget install -e --id Microsoft.WindowsTerminal
 winget install -e --id MoonlightGameStreamingProject.Moonlight
 winget install -e --id Notepad++.Notepad++
 winget install -e --id Obsidian.Obsidian
+winget install -e --id Parsec.Parsec
 winget install -e --id PicoTorrent.PicoTorrent
 winget install -e --id Pierre.Museeks
 winget install -e --id SublimeHQ.SublimeMerge
 winget install -e --id SublimeHQ.SublimeText.4
-winget install -e --id VideoLAN.VLC
-winget install -e --id eloston.ungoogled-chromium
-winget install -e --id Parsec.Parsec
-winget install -e --id MichaelTippach.ASIO4ALL
 winget install -e --id Valve.Steam
-winget install -e --id Lapce.Lapce
-winget install -e --id Microsoft.WindowsTerminal
-winget install -e --id Microsoft.VisualStudioCode
+winget install -e --id VideoLAN.VLC
 
 echo "Now install 'https://vb-audio.com/Cable/#DownloadASIOBridge'"
 echo "and other stuff from steam like aseprite"
