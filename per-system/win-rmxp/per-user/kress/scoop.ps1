cdc4215629bc3e87d1f64d2ffea67587b8cd166c
Chris <2013206+kress95@users.noreply.github.com>
2023-06-23 01:35:38 -0300

    bump

--- per-system/win-rmxp/per-user/kress/scoop.ps1
+++ per-system/win-rmxp/per-user/kress/scoop.ps1
@@ -8,7 +8,50 @@ scoop bucket add versions
 scoop bucket add kress95 https://github.com/kress95/scoop-bucket
 scoop bucket add insomnia https://github.com/insomnimus/scoop-bucket
 scoop update
-scoop install adb anydesk audacity dark Delugia-Nerd-Font-Complete deno discord ffmpeg gallery-dl git github glazewm godot hide-volume-osd innounp JetBrains-Mono JetBrainsMono-NF-Mono krita lmms micro moonlight notepadplusplus npiperelay obsidian picotorrent powertoys scoop-search starship sublime-text sudo thorium-avx2-np ungoogled-chromium vcredist2022 VcXsrv vlc volume2 vscode windows-terminal yt-dlp zoxide
-# taskbarx
-sudo scoop install --global asio4all-np equalizer-apo-np hide-volume-osd-np parsec-np steam
-echo "Now install other stuff from steam like aseprite"
+
+# cli
+scoop install 7zip
+scoop install 7zip19.00-helper
+scoop install adb
+scoop install bzip2
+scoop install curl
+# scoop install dark
+scoop install deno
+scoop install dprint
+scoop install ffmpeg
+scoop install gallery-dl
+scoop install gcc
+scoop install git
+scoop install gzip
+scoop install innounp
+scoop install just
+scoop install llvm
+scoop install micro
+scoop install mkvtoolnix
+scoop install nodejs16
+scoop install npiperelay
+scoop install oh-my-posh
+scoop install ps-dotenv
+scoop install scoop-search
+scoop install starship
+scoop install sudo
+scoop install tar
+scoop install unzip
+scoop install yt-dlp
+scoop install zoxide
+
+# apps
+scoop install thorium-avx2-np
+# scoop install taskbarx
+
+# global apps
+sudo scoop install --global equalizer-apo-np
+sudo scoop install --global hide-volume-osd-np
+sudo scoop install --global powertoys-np
+sudo scoop install --global office-365-apps-minimal-np
+
+# fonts
+scoop install Delugia-Nerd-Font-Complete
+scoop install Iosevka-NF-Mono
+scoop install JetBrains-Mono
+scoop install JetBrainsMono-NF-Mono
