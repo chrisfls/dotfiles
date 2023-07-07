e33bdfbc17cf7f609f938b5986fcfcdddd49239f
Chris <2013206+kress95@users.noreply.github.com>
2023-07-07 00:12:34 -0300

    bump confs

--- home/fish/shell_init.fish
+++ home/fish/shell_init.fish
@@ -5,7 +5,8 @@ any-nix-shell fish --info-right | source
 
 if not set -q tide_setup
   echo "tide setup init"
-  echo 1 2 1 1 2 1 y | tide configure >/dev/null
+  # echo 1 2 1 1 2 1 y | tide configure >/dev/null
+  echo 1 2 1 2 3 2 1 y | tide configure >/dev/null
   set --universal tide_character_icon "λ"
   set --universal tide_setup true
   echo "tide setup done"
