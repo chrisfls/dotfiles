5dcfd7b6c5341f29373b5d85fbf3ab45b35e6dde
Chris <2013206+kress95@users.noreply.github.com>
2023-08-12 01:54:02 -0300

    bump

--- per-system/win-rmxp/per-user/kress/scoop.ps1
+++ per-system/win-rmxp/per-user/kress/scoop.ps1
@@ -10,28 +10,28 @@ scoop bucket add insomnia https://github.com/insomnimus/scoop-bucket
 scoop update
 
 # cli
-# scoop install 7zip
+scoop install 7zip
 scoop install 7zip19.00-helper
 scoop install adb
 scoop install bzip2
 scoop install curl
 # scoop install dark
 scoop install deno
+scoop install dotnet-sdk-preview
 scoop install dprint
 scoop install ffmpeg
 scoop install gallery-dl
-scoop install gcc
+# scoop install gcc
 scoop install git
 scoop install gzip
-scoop install innounp
-scoop install just
-scoop install llvm
+# scoop install innounp
+# scoop install just
+# scoop install llvm
 scoop install micro
-scoop install mkvtoolnix
-scoop install nodejs16
-scoop install npiperelay
-scoop install oh-my-posh
-scoop install ps-dotenv
+# scoop install mkvtoolnix
+# scoop install nodejs16
+# scoop install npiperelay
+# scoop install ps-dotenv
 scoop install scoop-search
 scoop install starship
 scoop install sudo
@@ -43,14 +43,13 @@ scoop install yt-dlp
 scoop install zoxide
 
 # apps
-scoop install thorium-avx2-np
+# scoop install thorium-avx2-np
 # scoop install taskbarx
 
 # global apps
 sudo scoop install --global equalizer-apo-np
 sudo scoop install --global hide-volume-osd-np
 sudo scoop install --global powertoys-np
-sudo scoop install --global office-365-apps-minimal-np
 
 # fonts
 scoop install Delugia-Nerd-Font-Complete
--- per-system/win-rmxp/per-user/kress/winget.ps1
+++ per-system/win-rmxp/per-user/kress/winget.ps1
@@ -1,15 +1,21 @@
-winget install -e --id AnyDeskSoftwareGmbH.AnyDesk
-winget install -e --id Audacity.Audacity
-winget install -e --id AutoHotkey.AutoHotkey                                              
+# winget install -e --id GNU.Emacs
+# winget install -e --id Lapce.Lapce
+# winget install -e --id lars-berger.GlazeWM
+# winget install -e --id Ovenoboyo.Moosync
+# winget install -e --id Shabinder.SpotiFlyer
+# winget install -e --id staniel359.muffon
+# winget install -e --id SublimeHQ.SublimeMerge
+# winget install -e --id SublimeHQ.SublimeText.4
+winget install -e --id AutoHotkey.AutoHotkey
+winget install -e --id Brave.Brave
+winget install -e --id Cloudflare.Warp
+winget install -e --id deepnight.LDtk
 winget install -e --id Discord.Discord
 winget install -e --id eloston.ungoogled-chromium
 winget install -e --id GIMP.GIMP
-winget install -e --id GNU.Emacs
 winget install -e --id irzyxa.Volume2Portable
 winget install -e --id ItchIo.Itch
 winget install -e --id KDE.Krita
-winget install -e --id Lapce.Lapce
-winget install -e --id lars-berger.GlazeWM
 winget install -e --id LMMS.LMMS
 winget install -e --id Logitech.GHUB
 winget install -e --id Logitech.LogiBolt
@@ -17,23 +23,24 @@ winget install -e --id Logitech.OptionsPlus
 winget install -e --id marha.VcXsrv
 winget install -e --id MichaelTippach.ASIO4ALL
 winget install -e --id Microsoft.PowerShell
+winget install -e --id Microsoft.PowerToys
+winget install -e --id Microsoft.VisualStudio.2022.Community
 winget install -e --id Microsoft.VisualStudioCode
+winget install -e --id Microsoft.WindowsSDK.10.0.22000
 winget install -e --id Microsoft.WindowsTerminal
 winget install -e --id MoonlightGameStreamingProject.Moonlight
 winget install -e --id Notepad++.Notepad++
 winget install -e --id Obsidian.Obsidian
-winget install -e --id Ovenoboyo.Moosync
 winget install -e --id Parsec.Parsec
 winget install -e --id PicoTorrent.PicoTorrent
 winget install -e --id Pierre.Museeks
 winget install -e --id RamenSoftware.7+TaskbarTweaker
-winget install -e --id Shabinder.SpotiFlyer
+winget install -e --id Rclone.Rclone
 winget install -e --id Soulseek.SoulseekQt
-winget install -e --id staniel359.muffon
-winget install -e --id SublimeHQ.SublimeMerge
-winget install -e --id SublimeHQ.SublimeText.4
+winget install -e --id Tiled.Tiled
 winget install -e --id Valve.Steam
 winget install -e --id VideoLAN.VLC
+winget install -e --id WinFsp.WinFsp
 
 echo "Now install 'https://vb-audio.com/Cable/#DownloadASIOBridge'"
 echo "and other stuff from steam like aseprite"
