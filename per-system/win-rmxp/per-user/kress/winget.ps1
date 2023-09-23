b19b30a607fac5ca2975d5ebd39e01336d95bafb
Chris <2013206+kress95@users.noreply.github.com>
2023-09-23 18:19:06 -0300

    moving a bunch of stuff

--- per-system/win-rmxp/per-user/kress/winget.ps1
+++ per-system/win-rmxp/per-user/kress/winget.ps1
@@ -1,22 +1,38 @@
+# winget install -e --id deepnight.LDtk
 # winget install -e --id GNU.Emacs
 # winget install -e --id Lapce.Lapce
 # winget install -e --id lars-berger.GlazeWM
+# winget install -e --id Microsoft.CLRTypesSQLServer.2019
+# winget install -e --id Microsoft.Edge
+# winget install -e --id Microsoft.EdgeWebView2Runtime
+# winget install -e --id Microsoft.OneDrive
+# winget install -e --id Microsoft.VCRedist.2008.x64
+# winget install -e --id Microsoft.VCRedist.2008.x86
+# winget install -e --id Microsoft.VCRedist.2010.x64
+# winget install -e --id Microsoft.VCRedist.2010.x86
+# winget install -e --id Microsoft.VCRedist.2015+.x64
+# winget install -e --id Microsoft.VCRedist.2015+.x86
+# winget install -e --id Obsidian.Obsidian
 # winget install -e --id Ovenoboyo.Moosync
+# winget install -e --id Rclone.Rclone
 # winget install -e --id Shabinder.SpotiFlyer
 # winget install -e --id staniel359.muffon
 # winget install -e --id SublimeHQ.SublimeMerge
 # winget install -e --id SublimeHQ.SublimeText.4
-winget install -e --id AutoHotkey.AutoHotkey
+# winget install -e --id th-ch.YouTubeMusic
+winget install -e --id AnyDeskSoftwareGmbH.AnyDesk
+winget install -e --id Audacity.Audacity --location "D:\Program Files\Audacity"
+winget install -e --id AutoHotkey.AutoHotkey --location "D:\Program Files\AutoHotkey"
 winget install -e --id Brave.Brave
 winget install -e --id Cloudflare.Warp
-winget install -e --id deepnight.LDtk
-winget install -e --id Discord.Discord
-winget install -e --id eloston.ungoogled-chromium
-winget install -e --id GIMP.GIMP
+winget install -e --id Discord.Discord --location "D:\Program Files\Discord"
+winget install -e --id eloston.ungoogled-chromium --location "D:\Program Files\Ungoogled Chromium"
+winget install -e --id GIMP.GIMP.Nightly --location "D:\Program Files\GIMP Nightly"
 winget install -e --id irzyxa.Volume2Portable
-winget install -e --id ItchIo.Itch
-winget install -e --id KDE.Krita
-winget install -e --id LMMS.LMMS
+winget install -e --id ItchIo.Itch --location "D:\Program Files\Itch"
+winget install -e --id KDE.Krita --location "D:\Program Files\Krita (x64)"
+winget install -e --id LanguageTool.LanguageTool --location "D:\Program Files\LanguageTool"
+winget install -e --id LMMS.LMMS --location "D:\Program Files\LMMS"
 winget install -e --id Logitech.GHUB
 winget install -e --id Logitech.LogiBolt
 winget install -e --id Logitech.OptionsPlus
@@ -27,21 +43,18 @@ winget install -e --id Microsoft.PowerShell
 winget install -e --id Microsoft.PowerToys
 winget install -e --id Microsoft.VisualStudio.2022.Community
 winget install -e --id Microsoft.VisualStudioCode
-winget install -e --id Microsoft.VisualStudioCode
 winget install -e --id Microsoft.WindowsSDK.10.0.22000
 winget install -e --id Microsoft.WindowsTerminal
 winget install -e --id MoonlightGameStreamingProject.Moonlight
-winget install -e --id Notepad++.Notepad++
-winget install -e --id Obsidian.Obsidian
-winget install -e --id Parsec.Parsec
-winget install -e --id PicoTorrent.PicoTorrent
+winget install -e --id Notepad++.Notepad++ --location "D:\Program Files\Notepad++"
+winget install -e --id Parsec.Parsec --location "D:\Program Files\Parsec"
+winget install -e --id PicoTorrent.PicoTorrent --location "D:\Program Files\PicoTorrent"
 winget install -e --id Pierre.Museeks
 winget install -e --id RamenSoftware.7+TaskbarTweaker
-winget install -e --id Rclone.Rclone
 winget install -e --id Soulseek.SoulseekQt
-winget install -e --id Tiled.Tiled
-winget install -e --id Valve.Steam
-winget install -e --id VideoLAN.VLC
+winget install -e --id Tiled.Tiled --location "D:\Program Files\Tiled"
+winget install -e --id Valve.Steam --location "D:\Program Files (x86)\Steam"
+winget install -e --id VideoLAN.VLC --location "D:\Program Files\VideoLAN\VLC"
 winget install -e --id WinFsp.WinFsp
 
 echo "Now install 'https://vb-audio.com/Cable/#DownloadASIOBridge'"
