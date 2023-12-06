885aa184ba10574bc40ace7346bac38159cdbc35
Chris <2013206+kress95@users.noreply.github.com>
2023-12-06 20:30:31 -0300

    add nixVulkan

--- home/desk/arch.nix
+++ home/desk/arch.nix
@@ -8,5 +8,24 @@ in
 {
   home.packages = [
     overlay.nixgl.nixGLIntel
+    overlay.nixgl.nixVulkanIntel
   ];
+
+  programs.bash.sessionVariables.XDG_DATA_DIRS = "$HOME/.nix-profile/share:$XDG_DATA_DIRS";
+
+  # programs.bash.profileExtra = ''
+  #   source <(
+  #     nixGLIntel /bin/bash -c "\
+  #       echo \"export __EGL_VENDOR_LIBRARY_FILENAMES=\\\"\$__EGL_VENDOR_LIBRARY_FILENAMES:\$__EGL_VENDOR_LIBRARY_FILENAMES\\\"\"; \
+  #       echo \"export LD_LIBRARY_PATH=\\\"\$LD_LIBRARY_PATH:\$LD_LIBRARY_PATH\\\"\"; \
+  #       echo \"export LIBGL_DRIVERS_PATH=\\\"\$LIBGL_DRIVERS_PATH:\$LIBGL_DRIVERS_PATH\\\"\"; \
+  #       echo \"export LIBVA_DRIVERS_PATH=\\\"\$LIBVA_DRIVERS_PATH:\$LIBVA_DRIVERS_PATH\\\"\";"
+  #   )
+  #   source <(
+  #     libVulkanIntel /bin/bash -c "\
+  #       echo \"export LD_LIBRARY_PATH=\\\"\$LD_LIBRARY_PATH:\$LD_LIBRARY_PATH\\\"\"; \
+  #       echo \"export VK_ICD_FILENAMES=\\\"\$VK_ICD_FILENAMES:\$VK_ICD_FILENAMES\\\"\"; \
+  #       echo \"export VK_LAYER_PATH=\\\"\$VK_LAYER_PATH:\$VK_LAYER_PATH\\\"\";"
+  #   )
+  # '';
 }
