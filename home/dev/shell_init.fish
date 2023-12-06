d52f3a4691acb89f6d1c4e9934995cc289268a79
Chris <2013206+kress95@users.noreply.github.com>
2023-12-06 18:08:17 -0300

    fix tide setup

--- home/dev/shell_init.fish
+++ home/dev/shell_init.fish
@@ -5,7 +5,9 @@ any-nix-shell fish --info-right | source
 
 if not set -q tide_setup
   echo "tide setup init"
-  echo 1 2 1 2 3 2 1 1 y | tide configure >/dev/null
+  # replace with: (>= nixos-23.11)
+  # echo 1 2 1 2 3 2 1 1 y | tide configure >/dev/null
+  echo 1 2 1 2 3 2 1 y | tide configure >/dev/null
   set --universal tide_character_icon "λ"
   set --universal tide_setup true
   echo "tide setup done"
