ce264e52a27e55629c7fd548675c6eeafa628639
Chris <2013206+kress95@users.noreply.github.com>
2023-12-06 18:07:59 -0300

    rollback to 23.05 and dedup flakes

--- flake.nix
+++ flake.nix
@@ -2,29 +2,43 @@
   description = "kress's NixOS Flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+
     systems.url = "github:nix-systems/default";
+
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-utils.inputs.systems.follows = "systems";
+
+    flake-compat.url = "github:edolstra/flake-compat";
+
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+    pre-commit-hooks.inputs.nixpkgs.follows = "nixpkgs";
+    pre-commit-hooks.inputs.flake-utils.follows = "flake-utils";
+    pre-commit-hooks.inputs.flake-compat.follows = "flake-compat";
+
     devenv.url = "github:cachix/devenv";
+    devenv.inputs.nixpkgs.follows = "nixpkgs";
+    devenv.inputs.pre-commit-hooks.follows = "pre-commit-hooks";
+    devenv.inputs.flake-compat.follows = "flake-compat";
 
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixGL.url = "github:guibou/nixGL";
+    nixGL.inputs.nixpkgs.follows = "nixpkgs";
+    nixGL.inputs.flake-utils.follows = "flake-utils";
 
-    agenix = {
-      url = "github:ryantm/agenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    home-manager.url = "github:nix-community/home-manager/release-23.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    homeage = {
-      url = "github:jordanisaacs/homeage";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    agenix.url = "github:ryantm/agenix";
+    agenix.inputs.nixpkgs.follows = "nixpkgs";
+    agenix.inputs.home-manager.follows = "home-manager";
+
+    homeage.url = "github:jordanisaacs/homeage";
+    homeage.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   nixConfig = {
-    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
-    extra-substituters = "https://devenv.cachix.org";
+    extra-trusted-public-keys = "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs= devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://nix-community.cachix.org https://devenv.cachix.org";
   };
 
   outputs = { nixpkgs, devenv, home-manager, homeage, systems, ... }@inputs:
--- home/kress@arch-rmxp.nix
+++ home/kress@arch-rmxp.nix
@@ -4,7 +4,7 @@
   home.homeDirectory = ssot.users.arch-rmxp.kress.home;
 
   # before changing this value read the documentation for this option
-  home.stateVersion = "23.11";
+  home.stateVersion = "23.05";
 
   # let home manager install and manage itself
   programs.home-manager.enable = true;
