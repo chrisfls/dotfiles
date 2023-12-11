63825de3d82bcc2fb7e978145340f3a32a889de7
Chris <2013206+kress95@users.noreply.github.com>
2023-12-11 02:34:03 -0300

    fix helpers

--- home/desk/hypr/hyprland.nix
+++ home/desk/hypr/hyprland.nix
@@ -2,10 +2,11 @@
 with specialArgs;
 {
   config = {
-    xdg.configFile."hypr/hyprland.conf".text = replaceVars
+    xdg.configFile."hypr/hyprland.conf".text = my.replaceVars
       (lib.mapAttrs (name: hex: builtins.substring 1 (-1) hex) config.theme)
       (builtins.readFile ./hyprland.conf);
 
+    # TODO:
     /*
       xdg.configFile."hypr/hyprpaper.conf".text = ''
       preload = ${./wallpaper/background.png}
