5d781e4db93cf9380ebb4bedd4e21d5430aaf2fa
Chris <2013206+kress95@users.noreply.github.com>
2023-12-13 16:11:18 -0300

    remove paack keys

--- secrets.nix
+++ secrets.nix
@@ -1,6 +1,5 @@
-let ssot = import ./common/ssot.nix; in
+let ssot = import ./ssot.nix; in
 {
   "home/dev/dot-envrc.secret.age".publicKeys = [ ssot.keys.wsl.kress ssot.keys.arch-rmxp.kress ];
   "home/dev/dot-npmrc.age".publicKeys = [ ssot.keys.wsl.kress ssot.keys.arch-rmxp.kress ];
-  "home/work/paack/dot-envrc.secret.age".publicKeys = [ ssot.keys.wsl.kress ssot.keys.arch-rmxp.kress ];
 }
