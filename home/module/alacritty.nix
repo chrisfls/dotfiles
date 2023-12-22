8ff74e7ad0a60cd9ea12bf3de6527e5d58aef808
Chris <2013206+kress95@users.noreply.github.com>
2023-12-22 04:13:52 -0300

    alacritty opaque

--- home/module/alacritty.nix
+++ home/module/alacritty.nix
@@ -18,6 +18,7 @@ in
       settings = {
         # TODO: default font
         shell.program = "${pkgs.fish}/bin/fish";
+        window.opacity = 0.75;
         colors = {
           primary = {
             background = colors.background;
