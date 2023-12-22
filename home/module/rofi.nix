e187deee99783f61349cff774d09fb56c849aa73
Chris <2013206+kress95@users.noreply.github.com>
2023-12-22 04:13:07 -0300

    fix rofi dpi

--- home/module/rofi.nix
+++ home/module/rofi.nix
@@ -8,12 +8,22 @@ let
     rev = "0a55e154598466edb4352013798acc1d2f245306";
     sha256 = "sha256-YjyrxappcLDoh3++mtZqCyxQV2qeoNhhUy2XGwlyTng=";
   };
+
+  dpi = builtins.toString (builtins.floor config.extra.scaling.dpiScaled);
+
+  pkg = pkgs.symlinkJoin {
+    name = "rofi";
+    paths = [
+      (pkgs.writeShellScriptBin "rofi" "${pkgs.rofi}/bin/rofi -dpi ${dpi} $@")
+      pkgs.rofi
+    ];
+  };
 in
 {
   options.extra.rofi.enable = lib.mkEnableOption "Enable rofi module";
 
   config = lib.mkIf cfg.enable {
-    home.packages = [ pkgs.rofi pkgs.hostname ];
+    home.packages = [ pkg pkgs.hostname ];
 
     services.sxhkd.keybindings."super + Return" = "rofi -show drun -theme \"${config.xdg.configHome}/rofi/launchers/type-3/style-1.rasi\"";
 
@@ -23,6 +33,8 @@ in
       "fonts/Iosevka-Nerd-Font-Complete.ttf".source = "${rofi}/fonts/Iosevka-Nerd-Font-Complete.ttf";
       "fonts/JetBrains-Mono-Nerd-Font-Complete.ttf".source = "${rofi}/fonts/JetBrains-Mono-Nerd-Font-Complete.ttf";
     };
+
+    # xresources.properties."rofi.dpi" = config.extra.scaling.dpiScaled;
 
     xdg.configFile."rofi".source = "${rofi}/files";
   };
