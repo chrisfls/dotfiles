c9cf299f2b878f44d58b1e4eecd6960ef28b21b4
Chris <2013206+kress95@users.noreply.github.com>
2023-12-23 02:00:17 -0300

    disable shadows

--- home/module/picom.nix
+++ home/module/picom.nix
@@ -19,30 +19,15 @@ in
         backend = "glx";
         vsync = true;
 
+        # disable shadows
+        shadow = false;
+
         # fade animations -------------
         fade-delta = 8;
         fade-exclude = [ ];
         fade-in-step = 0.250000;
         fade-out-step = 0.250000;
         fading = true;
-
-        # shadows ---------------------
-        shadow = false;
-        crop-shadow-to-monitor = true;
-        shadow-exclude = [
-          "_GTK_FRAME_EXTENTS@:c"
-          "(bounding_shaped && !rounded_corners)"
-          "class_g ?= 'Notify-osd'"
-          "class_g ?= 'zoom'"
-          "class_g = 'Cairo-clock'"
-          "class_g = 'Conky'"
-          "name = 'cpt_frame_xcb_window'"
-          "name = 'Notification'"
-          "name = 'rect-overlay'"
-        ];
-        shadow-offset-x = -18;
-        shadow-offset-y = -18;
-        shadow-opacity = 0.500000;
 
         # rounded corners -------------
         corner-radius = 12;
@@ -59,12 +44,6 @@ in
         blur-strength = 4;
         # only blur rofi and alacritty
         blur-background-exclude = "!(class_g = 'Rofi' || class_g = 'Alacritty')";
-      };
-
-      # only enable shadows for normal windows and for polybar
-      wintypes = {
-        dock.shadow = true;
-        normal.shadow = true;
       };
     };
   };
