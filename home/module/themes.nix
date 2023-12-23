ebdb75e22d9492171ac50052ea40fd0125b77452
Chris <2013206+kress95@users.noreply.github.com>
2023-12-23 17:57:08 -0300

    rofi theme

--- home/module/themes.nix
+++ home/module/themes.nix
@@ -185,6 +185,11 @@ in
       default = specialArgs.color-schemes.material-dark-kde-alt;
       description = "Color scheme for polybar and other apps.";
     };
+
+    rofi = lib.mkOption {
+      type = lib.types.str;
+      default = "${config.xdg.configHome}/rofi/launchers/type-3/style-1.rasi";
+    };
   };
 
   config = lib.mkIf cfg.enable {
