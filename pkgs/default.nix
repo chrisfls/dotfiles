18f7932b9c98e6b780ff83788843ef9c342f13ac
Chris <2013206+kress95@users.noreply.github.com>
2024-01-01 01:23:38 -0300

    kek

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -6,4 +6,5 @@ in
   sublime4 = callPackage (import ./sublime4.nix);
   sublime4-dev = callPackage (import ./sublime4-dev.nix);
   sublime-merge-dev = callPackage (import ./sublime-merge-dev.nix);
+  sublime-merge = callPackage (import ./sublime-merge.nix);
 }
