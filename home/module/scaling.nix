47ab314f3e46f72a0ffc06a22196ad280046590c
Chris <2013206+kress95@users.noreply.github.com>
2024-01-01 14:47:18 -0300

    fix most scaling issues

--- home/module/scaling.nix
+++ home/module/scaling.nix
@@ -2,8 +2,11 @@
 let
   cfg = config.module.scaling;
 
-  scale = builtins.ceil cfg.scale;
-  gdk-dpi-scale = cfg.scale / scale;
+  dpi = cfg.dpi;
+  scale = cfg.scale;
+  dpi-scaled = builtins.floor (dpi * scale);
+  gdk-scale = builtins.ceil scale;
+  gdk-dpi-scale = 1.0 / gdk-scale;
 in
 {
   options.module.scaling = {
@@ -18,26 +21,22 @@ in
       type = lib.types.int;
       default = 96;
     };
-
-    dpiScaled = lib.mkOption {
-      type = lib.types.int;
-      default = cfg.dpi * scale;
-    };
   };
 
   # when using GDK/QT scaling, the DPI must be hardcoded
   config = lib.mkIf cfg.enable {
-    xresources.properties."Xft.dpi" = toString config.module.scaling.dpiScaled;
+    xresources.properties."Xft.dpi" = toString dpi-scaled;
 
     home.sessionVariables = {
-      GDK_SCALE = toString 1;
-      GDK_DPI_SCALE = toString gdk-dpi-scale;
-
       QT_AUTO_SCREEN_SCALE_FACTOR = toString 0;
       QT_ENABLE_HIGHDPI_SCALING = toString 1;
-      QT_SCALE_FACTOR = toString cfg.scale;
+      QT_SCALE_FACTOR = toString scale;
 
-      QT_FONT_DPI = toString cfg.dpi;
+      QT_FONT_DPI = toString dpi;
+
+      # double size of icons (slows down gtk3 rendering a bit)
+      GDK_SCALE = toString gdk-scale;
+      GDK_DPI_SCALE = toString gdk-dpi-scale;
     };
   };
 }
