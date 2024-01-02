094370b1c4caa6fdcb96dc0602c11576a9350aa5
Chris <2013206+kress95@users.noreply.github.com>
2024-01-02 01:10:11 -0300

    fast mouse warp and autotiling

--- home/module/i3wm.nix
+++ home/module/i3wm.nix
@@ -1,23 +1,53 @@
 { config, lib, pkgs, ... }:
 let
+  inherit (builtins) concatStringsSep;
+
   cfg = config.module.i3wm;
 
   colors = config.module.themes.color-scheme;
 
+  script = name: "exec \"$SCRIPT/${name}\"";
+
+  i3-msg = "${pkgs.i3}/bin/i3-msg";
+
+  jaq = "${pkgs.jaq}/bin/jaq";
+
+  xdotool = "${pkgs.xdotool}/bin/xdotool";
+
   mod = config.xsession.windowManager.i3.config.modifier;
-
-  mouse-warp-pkg = pkgs.writeShellScriptBin "mouse-warp"
-    ''
-      eval $(${pkgs.xdotool}/bin/xdotool getwindowfocus getwindowgeometry --shell)
-      ${pkgs.xdotool}/bin/xdotool mousemove $((X + (WIDTH / 2))) $((Y + (HEIGHT / 2)))
-    '';
-
-  mouse-warp = "exec \"${mouse-warp-pkg}/bin/mouse-warp\"";
 in
 {
   options.module.i3wm.enable = lib.mkEnableOption "Enable i3wm module";
 
   config = lib.mkIf cfg.enable {
+    module.script.enable = true;
+
+    home.packages = [
+      pkgs.xdotool
+      pkgs.xorg.xkill
+    ];
+
+    module.script.install = {
+      autotile =
+        ''
+          ${i3-msg} -t subscribe -m '[ "window", "binding" ]' | while IFS= read -r line; do
+            eval $(${xdotool} getwindowfocus getwindowgeometry --shell)
+
+            if [ "$WIDTH" -gt "$HEIGHT" ]; then
+              ${i3-msg} "split horizontal"
+            else
+              ${i3-msg} "split vertical"
+            fi
+          done
+        '';
+
+      mouse-warp =
+        ''
+          eval $(${xdotool} getwindowfocus getwindowgeometry --shell)
+          ${xdotool} mousemove $((X + (WIDTH / 2))) $((Y + (HEIGHT / 2)))
+        '';
+    };
+
     xsession.windowManager.i3 = {
       enable = true;
       config = {
@@ -31,55 +61,55 @@ in
             background = colors.foreground;
             border = colors.foreground;
             childBorder = colors.foreground;
-            indicator = colors.blueBright;
+            indicator = colors.foreground;
             text = colors.background;
           };
           focusedInactive = {
-            background = colors.blackBright;
-            border = colors.blackBright;
-            childBorder = colors.blackBright;
-            indicator = colors.blue;
+            background = colors.black;
+            border = colors.black;
+            childBorder = colors.black;
+            indicator = colors.black;
             text = colors.background;
           };
           placeholder = {
             background = colors.background;
             border = colors.background;
             childBorder = colors.background;
-            indicator = colors.blue;
+            indicator = colors.background;
             text = colors.foreground;
           };
           unfocused = {
             background = colors.background;
             border = colors.background;
             childBorder = colors.background;
-            indicator = colors.blue;
+            indicator = colors.background;
             text = colors.black;
           };
           urgent = {
             background = colors.background;
             border = colors.background;
             childBorder = colors.background;
-            indicator = colors.blue;
+            indicator = colors.background;
             text = colors.yellow;
           };
         };
         defaultWorkspace = "1";
         floating = {
-          border = 1;
+          border = 2;
           criteria = [ ];
           modifier = mod;
           titlebar = false;
         };
         focus = {
           followMouse = false;
-          newWindow = "smart";
-          wrapping = "yes";
+          newWindow = "focus";
+          wrapping = "workspace";
         };
         /*fonts = {
             names = [ "DejaVu Sans Mono" "FontAwesome5Free" ];
             style = "Bold Semi-Condensed";
             size = 11.0;
-          };*/
+            };*/
         gaps = {
           top = 0;
           bottom = 0;
@@ -125,9 +155,15 @@ in
             "${mod}+i" = "mode default";
           };
         };
-        startup = [ ];
+        startup = [
+          {
+            command = "$SCRIPT/autotile";
+            always = true;
+            notification = false;
+          }
+        ];
         window = {
-          border = 1;
+          border = 2;
           /*
           commands = [
             {
@@ -137,7 +173,7 @@ in
               };
             }
           ];
-          */
+              */
           hideEdgeBorders = "smart";
           titlebar = false;
         };
@@ -152,11 +188,10 @@ in
           "${mod}+Escape" = "reload";
 
           # restart wm
-          "${mod}+Shift+r" = "restart";
+          "${mod}+Shift+Escape" = "restart";
 
-          # quit session
-          "${mod}+Shift+e" =
-            "exec i3-nagbar -t warning -m 'Do you want to exit i3?' -b 'Yes' 'i3-msg exit'";
+          # quit sessionss
+          "Control+Mod1+Escape" = "exec i3-nagbar -t warning -m 'Do you want to exit i3?' -b 'Yes' 'i3-msg exit'";
 
           # TODO: move to modules
           "${mod}+BackSpace" = "exec ${config.xsession.windowManager.i3.config.terminal}";
@@ -183,13 +218,13 @@ in
           # ## #
 
           # cycle [t]iled/monocle layout
-          "${mod}+t" = "focus child; layout tabbed";
+          "${mod}+t" = "focus child; layout toggle tabbed split";
 
           # toggle [f]loating state
           "${mod}+f" = "floating toggle";
 
           # [r]otate layout
-          "${mod}+r" = "layout toggle splitv splith";
+          "${mod}+r" = "focus child; layout toggle splitv splith";
 
           "${mod}+s" = "mode resize";
           "${mod}+i" = "mode insert";
@@ -201,6 +236,8 @@ in
           # TODO: toggle desktop
 
           "${mod}+p" = "focus parent";
+          "${mod}+Shift+p" = "focus child";
+
           "${mod}+g" = "gaps inner toggle; gaps outer toggle";
 
           ######## #### ## #
@@ -208,61 +245,61 @@ in
           ######## #### ## #
 
           # focus with vim keys
-          "${mod}+h" = "focus left; ${mouse-warp}";
-          "${mod}+j" = "focus down; ${mouse-warp}";
-          "${mod}+k" = "focus up; ${mouse-warp}";
-          "${mod}+l" = "focus right; ${mouse-warp}";
+          "${mod}+h" = "focus left; ${script "mouse-warp"}";
+          "${mod}+j" = "focus down; ${script "mouse-warp"}";
+          "${mod}+k" = "focus up; ${script "mouse-warp"}";
+          "${mod}+l" = "focus right; ${script "mouse-warp"}";
 
           # focus with arrow keys
-          "${mod}+Left" = "focus left; ${mouse-warp}";
-          "${mod}+Down" = "focus down; ${mouse-warp}";
-          "${mod}+Up" = "focus up; ${mouse-warp}";
-          "${mod}+Right" = "focus right; ${mouse-warp}";
+          "${mod}+Left" = "focus left; ${script "mouse-warp"}";
+          "${mod}+Down" = "focus down; ${script "mouse-warp"}";
+          "${mod}+Up" = "focus up; ${script "mouse-warp"}";
+          "${mod}+Right" = "focus right; ${script "mouse-warp"}";
 
           # move with vim keys
-          "${mod}+Shift+h" = "mark swap; focus left; swap container with mark swap; [con_mark=\"swap\"] focus; unmark swap; ${mouse-warp}";
-          "${mod}+Shift+j" = "mark swap; focus down; swap container with mark swap; [con_mark=\"swap\"] focus; unmark swap; ${mouse-warp}";
-          "${mod}+Shift+k" = "mark swap; focus up; swap container with mark swap; [con_mark=\"swap\"] focus; unmark swap; ${mouse-warp}";
-          "${mod}+Shift+l" = "mark swap; focus right; swap container with mark swap; [con_mark=\"swap\"] focus; unmark swap; ${mouse-warp}";
+          "${mod}+Shift+h" = "mark swap; focus left; swap container with mark swap; [con_mark=\"swap\"] focus; unmark swap; ${script "mouse-warp"}";
+          "${mod}+Shift+j" = "mark swap; focus down; swap container with mark swap; [con_mark=\"swap\"] focus; unmark swap; ${script "mouse-warp"}";
+          "${mod}+Shift+k" = "mark swap; focus up; swap container with mark swap; [con_mark=\"swap\"] focus; unmark swap; ${script "mouse-warp"}";
+          "${mod}+Shift+l" = "mark swap; focus right; swap container with mark swap; [con_mark=\"swap\"] focus; unmark swap; ${script "mouse-warp"}";
 
           # move with arrow keys
-          "${mod}+Shift+Left" = "mark swap; focus left; swap container with mark swap; [con_mark=\"swap\"] focus; unmark swap; ${mouse-warp}";
-          "${mod}+Shift+Down" = "mark swap; focus down; swap container with mark swap; [con_mark=\"swap\"] focus; unmark swap; ${mouse-warp}";
-          "${mod}+Shift+Up" = "mark swap; focus up; swap container with mark swap; [con_mark=\"swap\"] focus; unmark swap; ${mouse-warp}";
-          "${mod}+Shift+Right" = "mark swap; focus right; swap container with mark swap; [con_mark=\"swap\"] focus; unmark swap; ${mouse-warp}";
+          "${mod}+Shift+Left" = "mark swap; focus left; swap container with mark swap; [con_mark=\"swap\"] focus; unmark swap; ${script "mouse-warp"}";
+          "${mod}+Shift+Down" = "mark swap; focus down; swap container with mark swap; [con_mark=\"swap\"] focus; unmark swap; ${script "mouse-warp"}";
+          "${mod}+Shift+Up" = "mark swap; focus up; swap container with mark swap; [con_mark=\"swap\"] focus; unmark swap; ${script "mouse-warp"}";
+          "${mod}+Shift+Right" = "mark swap; focus right; swap container with mark swap; [con_mark=\"swap\"] focus; unmark swap; ${script "mouse-warp"}";
 
           # focus floating windows
-          "${mod}+space" = "focus mode_toggle";
+          "${mod}+space" = "focus mode_toggle; ${script "mouse-warp"}";
           "${mod}+o" = "mark subwindow; focus parent; focus parent; mark parent; [con_mark=\"subwindow\"] focus; move window to mark parent; [con_mark=\"subwindow\"] focus; unmark";
 
           ######## #### ## #
           # WORKSPACES
           ######## #### ## #
 
-          "${mod}+apostrophe" = "workspace number 0; ${mouse-warp}";
-          "${mod}+1" = "workspace number 1; ${mouse-warp}";
-          "${mod}+2" = "workspace number 2; ${mouse-warp}";
-          "${mod}+3" = "workspace number 3; ${mouse-warp}";
-          "${mod}+4" = "workspace number 4; ${mouse-warp}";
-          "${mod}+5" = "workspace number 5; ${mouse-warp}";
-          "${mod}+6" = "workspace number 6; ${mouse-warp}";
-          "${mod}+7" = "workspace number 7; ${mouse-warp}";
-          "${mod}+8" = "workspace number 8; ${mouse-warp}";
-          "${mod}+9" = "workspace number 9; ${mouse-warp}";
-          "${mod}+0" = "workspace number 10; ${mouse-warp}";
+          "${mod}+apostrophe" = "workspace number 0; ${script "mouse-warp"}";
+          "${mod}+1" = "workspace number 1; ${script "mouse-warp"}";
+          "${mod}+2" = "workspace number 2; ${script "mouse-warp"}";
+          "${mod}+3" = "workspace number 3; ${script "mouse-warp"}";
+          "${mod}+4" = "workspace number 4; ${script "mouse-warp"}";
+          "${mod}+5" = "workspace number 5; ${script "mouse-warp"}";
+          "${mod}+6" = "workspace number 6; ${script "mouse-warp"}";
+          "${mod}+7" = "workspace number 7; ${script "mouse-warp"}";
+          "${mod}+8" = "workspace number 8; ${script "mouse-warp"}";
+          "${mod}+9" = "workspace number 9; ${script "mouse-warp"}";
+          "${mod}+0" = "workspace number 10; ${script "mouse-warp"}";
           "${mod}+m" = "scratchpad show";
 
-          "${mod}+Shift+apostrophe" = "move container to workspace number 0; ${mouse-warp}";
-          "${mod}+Shift+1" = "move container to workspace number 1; ${mouse-warp}";
-          "${mod}+Shift+2" = "move container to workspace number 2; ${mouse-warp}";
-          "${mod}+Shift+3" = "move container to workspace number 3; ${mouse-warp}";
-          "${mod}+Shift+4" = "move container to workspace number 4; ${mouse-warp}";
-          "${mod}+Shift+5" = "move container to workspace number 5; ${mouse-warp}";
-          "${mod}+Shift+6" = "move container to workspace number 6; ${mouse-warp}";
-          "${mod}+Shift+7" = "move container to workspace number 7; ${mouse-warp}";
-          "${mod}+Shift+8" = "move container to workspace number 8; ${mouse-warp}";
-          "${mod}+Shift+9" = "move container to workspace number 9; ${mouse-warp}";
-          "${mod}+Shift+0" = "move container to workspace number 10; ${mouse-warp}";
+          "${mod}+Shift+apostrophe" = "move container to workspace number 0; ${script "mouse-warp"}";
+          "${mod}+Shift+1" = "move container to workspace number 1; ${script "mouse-warp"}";
+          "${mod}+Shift+2" = "move container to workspace number 2; ${script "mouse-warp"}";
+          "${mod}+Shift+3" = "move container to workspace number 3; ${script "mouse-warp"}";
+          "${mod}+Shift+4" = "move container to workspace number 4; ${script "mouse-warp"}";
+          "${mod}+Shift+5" = "move container to workspace number 5; ${script "mouse-warp"}";
+          "${mod}+Shift+6" = "move container to workspace number 6; ${script "mouse-warp"}";
+          "${mod}+Shift+7" = "move container to workspace number 7; ${script "mouse-warp"}";
+          "${mod}+Shift+8" = "move container to workspace number 8; ${script "mouse-warp"}";
+          "${mod}+Shift+9" = "move container to workspace number 9; ${script "mouse-warp"}";
+          "${mod}+Shift+0" = "move container to workspace number 10; ${script "mouse-warp"}";
           "${mod}+shift+m" = "move scratchpad";
         };
       };
@@ -285,7 +322,7 @@ in
           reverse-scroll = "\"false\"";
           fuzzy-match = "\"false\"";
 
-          format = "\"%{O-1}%{O2}%{T2}<label-state>%{T-}%{}%{O-1}\"";
+          format = "\"%{O-1}%{O2}%{T2}<label-state>%{T-}%{O-1}\"";
           label-mode = "%{T2}%mode%%{T-}";
           label-mode-background = "\"${colors.foreground}\"";
           label-mode-foreground = "\"${colors.background}\"";
