e42fd08250b2ce9cb99a426c243c8be3b063ddae
Chris <2013206+kress95@users.noreply.github.com>
2024-01-03 14:18:32 -0300

    cleanup code

--- home/module/pacman.nix
+++ home/module/pacman.nix
@@ -1,17 +1,23 @@
 # TODO: add aur support
 { config, lib, pkgs, ... }:
 let
+  inherit (builtins) concatStringsSep filter isAttrs isList listToAttrs;
   inherit (config.pacman) enable overrides packages;
+  inherit (lib.attrsets) attrValues getAttrFromPath hasAttrByPath mapAttrsRecursive recursiveUpdate;
+  inherit (lib.lists) concatMap foldl';
+  inherit (lib.trivial) pipe;
+  inherit (lib) mkEnableOption mkIf mkOption mkOptionType types;
+
 
   # base package to copy files from pacman into nix
-  package = prev: path: _:
+  package = prev: path:
     let
       pkg = {
         pname = "pacman";
         version = "dummy";
         meta = { };
         passthru = { };
-      } // (lib.attrsets.getAttrFromPath path prev);
+      } // (getAttrFromPath path prev);
 
       replacer = prev.writeShellScriptBin "replace" ''
         set -eu
@@ -32,56 +38,59 @@ let
       '';
     };
 
+  # mapAttrsRecursive without value
+  mapPathsRecursive = fn: mapAttrsRecursive (path: _: fn path);
+
   # merge replace overrides with packages and merges it into prev
-  overlay = prev: lib.attrsets.recursiveUpdate prev
-    (lib.attrsets.mapAttrsRecursive (package prev) overrides);
+  overlay = prev: recursiveUpdate prev
+    (mapPathsRecursive (package prev) overrides);
 
   # install or update all packages required by nix  
   pacman-switch-pkg = pkgs.writeShellScriptBin "pacman-switch"
-    (builtins.concatStringsSep " && "
+    (concatStringsSep " && "
       [
         "sudo pacman -Sy --needed --noconfirm archlinux-keyring chaotic-keyring"
-        "sudo pacman -Su --needed --noconfirm ${builtins.concatStringsSep " " packages}"
+        "sudo pacman -Su --needed --noconfirm ${concatStringsSep " " packages}"
       ]
     );
 
   # fetches all the leafs of an attrset
   attrLeafs = attrs:
-    lib.lists.concatMap
+    concatMap
       (value:
-        if builtins.isAttrs value then
+        if isAttrs value then
           attrLeafs value
-        else if builtins.isList value then
+        else if isList value then
           value
         else
           [ value ])
-      (lib.attrsets.attrValues attrs);
+      (attrValues attrs);
 
   concatLines =
-    builtins.concatStringsSep "\n";
+    concatStringsSep "\n";
 in
 {
   options.pacman = {
-    enable = lib.mkEnableOption "Enable pacman module";
+    enable = mkEnableOption "Enable pacman module";
 
-    overrides = lib.mkOption {
-      type = lib.mkOptionType {
+    overrides = mkOption {
+      type = mkOptionType {
         name = "attrs";
         description = "attribute set";
-        check = lib.isAttrs;
-        merge = loc: lib.lists.foldl' (res: def: lib.attrsets.recursiveUpdate res def.value) { };
+        check = isAttrs;
+        merge = loc: foldl' (res: def: recursiveUpdate res def.value) { };
         emptyValue = { value = { }; };
       };
       default = { };
     };
 
-    packages = lib.mkOption {
-      type = lib.types.listOf lib.types.str;
+    packages = mkOption {
+      type = types.listOf types.str;
       default = [ ];
     };
   };
 
-  config = lib.mkIf enable {
+  config = mkIf enable {
     home.packages = [ pacman-switch-pkg ];
     nixpkgs.overlays = [ (final: overlay) ];
     pacman.packages = attrLeafs overrides;
@@ -89,22 +98,22 @@ in
     # report packages installed with nix (debug)
     home.file.".nixpkgs".text =
       let
-        packages-by-name = lib.trivial.pipe overrides [
-          (lib.attrsets.mapAttrsRecursive (path: _: (lib.attrsets.getAttrFromPath path pkgs).name))
+        packages-by-name = pipe overrides [
+          (mapPathsRecursive (path: (getAttrFromPath path pkgs).name))
           attrLeafs
           (map (name: { inherit name; value = true; }))
-          builtins.listToAttrs
+          listToAttrs
         ];
       in
-      lib.trivial.pipe config.home.packages [
+      pipe config.home.packages [
         (map (pkg: "${pkg.name}"))
-        (builtins.filter (name: !(lib.attrsets.hasAttrByPath [ name ] packages-by-name)))
+        (filter (name: !(hasAttrByPath [ name ] packages-by-name)))
         concatLines
       ];
 
     home.file.".pacman".text =
-      lib.trivial.pipe overrides [
-        (lib.attrsets.mapAttrsRecursive (path: _: [ (builtins.concatStringsSep "." path) ]))
+      pipe overrides [
+        (mapPathsRecursive (path: (concatStringsSep "." path)))
         attrLeafs
         concatLines
       ];
