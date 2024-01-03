307b26f6122ec228ac357e6a8d05ff67f38faa82
Chris <2013206+kress95@users.noreply.github.com>
2024-01-03 14:07:48 -0300

    fix nested props

--- home/module/pacman.nix
+++ home/module/pacman.nix
@@ -3,6 +3,7 @@
 let
   inherit (config.pacman) enable overrides packages;
 
+  # base package to copy files from pacman into nix
   package = prev: path: _:
     let
       pkg = {
@@ -31,41 +32,46 @@ let
       '';
     };
 
+  # merge replace overrides with packages and merges it into prev
   overlay = prev: lib.attrsets.recursiveUpdate prev
     (lib.attrsets.mapAttrsRecursive (package prev) overrides);
 
-  flatten = attrs:
-    lib.lists.concatMap
-      (value: if builtins.isAttrs value then flatten value else value)
-      (lib.attrsets.attrValues attrs);
-
-  packages-by-name = lib.trivial.pipe overrides [
-    (lib.attrsets.mapAttrsRecursive
-      (path:
-        let inherit (lib.attrsets.getAttrFromPath path pkgs) name;
-        in _: [ "${name}" ]))
-    flatten
-    (map (name: { inherit name; value = true; }))
-    builtins.listToAttrs
-  ];
-
-  pacman-packages =
-    builtins.concatStringsSep " " config.pacman.packages;
-
-  pacman-switch = pkgs.writeShellScriptBin "pacman-switch"
+  # install or update all packages required by nix  
+  pacman-switch-pkg = pkgs.writeShellScriptBin "pacman-switch"
     (builtins.concatStringsSep " && "
       [
         "sudo pacman -Sy --needed --noconfirm archlinux-keyring chaotic-keyring"
-        "sudo pacman -Su --needed --noconfirm ${pacman-packages}"
+        "sudo pacman -Su --needed --noconfirm ${builtins.concatStringsSep " " packages}"
       ]
     );
+
+  # fetches all the leafs of an attrset
+  attrLeafs = attrs:
+    lib.lists.concatMap
+      (value:
+        if builtins.isAttrs value then
+          attrLeafs value
+        else if builtins.isList value then
+          value
+        else
+          [ value ])
+      (lib.attrsets.attrValues attrs);
+
+  concatLines =
+    builtins.concatStringsSep "\n";
 in
 {
   options.pacman = {
     enable = lib.mkEnableOption "Enable pacman module";
 
     overrides = lib.mkOption {
-      type = lib.types.attrs;
+      type = lib.mkOptionType {
+        name = "attrs";
+        description = "attribute set";
+        check = lib.isAttrs;
+        merge = loc: lib.lists.foldl' (res: def: lib.attrsets.recursiveUpdate res def.value) { };
+        emptyValue = { value = { }; };
+      };
       default = { };
     };
 
@@ -76,16 +82,31 @@ in
   };
 
   config = lib.mkIf enable {
-    home.packages = [ pacman-switch ];
+    home.packages = [ pacman-switch-pkg ];
     nixpkgs.overlays = [ (final: overlay) ];
-    pacman.packages = (flatten overrides);
+    pacman.packages = attrLeafs overrides;
 
     # report packages installed with nix (debug)
     home.file.".nixpkgs".text =
+      let
+        packages-by-name = lib.trivial.pipe overrides [
+          (lib.attrsets.mapAttrsRecursive (path: _: (lib.attrsets.getAttrFromPath path pkgs).name))
+          attrLeafs
+          (map (name: { inherit name; value = true; }))
+          builtins.listToAttrs
+        ];
+      in
       lib.trivial.pipe config.home.packages [
         (map (pkg: "${pkg.name}"))
         (builtins.filter (name: !(lib.attrsets.hasAttrByPath [ name ] packages-by-name)))
-        (builtins.concatStringsSep "\n")
+        concatLines
+      ];
+
+    home.file.".pacman".text =
+      lib.trivial.pipe overrides [
+        (lib.attrsets.mapAttrsRecursive (path: _: [ (builtins.concatStringsSep "." path) ]))
+        attrLeafs
+        concatLines
       ];
   };
 }
