023a068a278bcbaa9d81ba675da2a2ac213671bf
Chris <2013206+kress95@users.noreply.github.com>
2024-01-11 01:39:31 -0300

    remove arch side fonts

--- home/module/fonts.nix
+++ home/module/fonts.nix
@@ -3,20 +3,6 @@ let inherit (config.module.fonts) enable; in {
   options.module.fonts.enable = lib.mkEnableOption "Enable fonts module";
 
   config = lib.mkIf enable {
-    pacman.packages = lib.mkIf config.preset.non-nixos [
-      "extra/noto-fonts"
-      "extra/noto-fonts-emoji"
-
-      "extra/ttf-montserrat"
-      "extra/ttf-overpass"
-
-      "extra/ttf-cascadia-code-nerd"
-      "extra/ttf-cascadia-code"
-
-      "extra/ttf-jetbrains-mono-nerd"
-      "extra/ttf-jetbrains-mono"
-    ];
-
     home.packages = [
       # good generic font
       pkgs.noto-fonts
