8c3b55da2cd915a1cfa26ca68d629847c20b81b1
Chris <2013206+kress95@users.noreply.github.com>
2024-01-11 10:09:29 -0300

    bump el config

--- assets/doom/config.el
+++ assets/doom/config.el
@@ -21,7 +21,7 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-(setq doom-font (font-spec :family "Cascadia Mono" :size 12.0 :weight 'semi-light)
+(setq doom-font (font-spec :family "Cascadia Mono" :size 12.0) ; :weight 'semi-light
       doom-variable-pitch-font (font-spec :family "Noto Sans" :size 13.0))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
