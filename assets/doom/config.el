2c4afdc4cd3e219eb17742b330e7f62962164581
Chris <2013206+kress95@users.noreply.github.com>
2024-01-20 19:18:54 -0300

    organize settings

--- assets/doom/config.el
+++ assets/doom/config.el
@@ -1,37 +1,47 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-(setq user-full-name "Christian Ferraz"
-      user-mail-address "")
-
-(setq org-directory "~/Desktop/org/")
-
-;;doom-big-font
-;;doom-symbol-font
-;;doom-serif-font
-(setq doom-font (font-spec :family "Jetbrains Mono NFM" :size (* 12.0 1.5)) ; :weight 'semi-light
-      doom-variable-pitch-font (font-spec :family "Noto Sans" :size (* 13.0 1.5)))
-      
-(setq doom-theme 'doom-outrun-electric ; 'doom-gruvbox 'doom-dracula 'doom-monokai-classic 'doom-tokyo-night
-      doom-gruvbox-dark-variant "hard"
-      +doom-dashboard-ascii-banner-fn nil
-      +word-wrap-extra-indent 'single)
-
-(setq ;display-line-numbers-type 'relative
-      mouse-wheel-follow-mouse 't
-      mouse-wheel-progressive-speed nil
-      mouse-wheel-scroll-amount '(1 ((shift) . 1))
-      ring-bell-function 'ignore
-      scroll-margin 3
-      scroll-step 1
-      scroll-step 3
-      blink-cursor-blinks 0)
-
 (setq-default cursor-type '(bar . 4)
               display-fill-column-indicator-column 79
-              display-fill-column-indicator-character ?·
+              display-fill-column-indicator-character ?\s
               fill-column 80)
 
+;; TODO: doom-big-font doom-symbol-font doom-serif-font
+(setq user-full-name "Christian Ferraz"
+      user-mail-address ""
+      org-directory "~/Desktop/org/"
+      doom-font (font-spec :family "Jetbrains Mono NFM" :size (* 12.0 1.5)) ; :weight 'semi-light
+      doom-variable-pitch-font (font-spec :family "Noto Sans" :size (* 13.0 1.5))
+      doom-theme 'doom-outrun-electric ; 'doom-gruvbox 'doom-dracula 'doom-monokai-classic 'doom-tokyo-night
+      doom-gruvbox-dark-variant "hard"
+      +doom-dashboard-ascii-banner-fn nil
+      +word-wrap-extra-indent 'single
+      mouse-wheel-follow-mouse 't
+      mouse-wheel-progressive-speed nil
+      mouse-wheel-scroll-amount '(3 ((shift) . 3))
+      ring-bell-function 'ignore
+      blink-cursor-blinks 0
+      scroll-conservatively 10000
+      scroll-margin 3
+      scroll-preserve-screen-position 1
+      scroll-step 3)
+
+(after! highlight-indent-guides
+  (setq highlight-indent-guides-method 'column
+        highlight-indent-guides-responsive 'top
+        highlight-indent-guides-auto-odd-face-perc 25
+        highlight-indent-guides-auto-even-face-perc 25
+        highlight-indent-guides-auto-character-face-perc 50)
+
+  ;; TODO: support light themes
+  (set-face-attribute 'fill-column-indicator nil
+    :background (face-attribute 'highlight-indent-guides-even-face
+                                :background nil
+                                'default)
+    :foreground nil))
+
+(after! meow
+  (add-to-list '+modeline-format-left '(:eval (meow-indicator))))
+
+(+global-word-wrap-mode 1)
 (blink-cursor-mode 1)
 (global-display-fill-column-indicator-mode 1)
-(pixel-scroll-precision-mode 1)
-(+global-word-wrap-mode 1)
