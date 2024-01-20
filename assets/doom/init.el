bfbc4e71ff3182ab1e56ab4f4c67100570da7de0
Chris <2013206+kress95@users.noreply.github.com>
2024-01-20 13:26:59 -0300

    trim configs a bit

--- assets/doom/init.el
+++ assets/doom/init.el
@@ -21,34 +21,34 @@
        ;;layout            ; auie,ctsrnm is the superior home row
 
        :completion
-       company          ; the ultimate code completion backend
-       ;;helm             ; the *other* search engine for love and life
-       ;;ido              ; the other *other* search engine...
-       ;;ivy              ; a search engine for love and life
+       (company +childframe) ; the ultimate code completion backend
+       ;;helm                  ; the *other* search engine for love and life
+       ;;ido                   ; the other *other* search engine...
+       ;;ivy                   ; a search engine for love and life
        (vertico +childframe
-                +icons) ; the search engine of the future
+                +icons)      ; the search engine of the future
 
        :ui
        ;;deft                ; notational velocity for Emacs
        doom                ; what makes DOOM look the way it does
        doom-dashboard      ; a nifty splash screen for Emacs
        ;;doom-quit           ; DOOM quit-message prompts when you quit Emacs
-       (emoji +unicode
-              +github)     ; 🙂
+       (emoji +github)     ; 🙂
        hl-todo             ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;hydra
        indent-guides       ; highlighted indent columns
        ligatures           ; ligatures and symbols to make your code pretty again
        ;;minimap             ; show a map of the code on the side
-       modeline            ; snazzy, Atom-inspired modeline, plus API
+       (modeline +light)   ; snazzy, Atom-inspired modeline, plus API
        nav-flash           ; blink cursor line after big motions
        ;;neotree             ; a project drawer, like NERDTree for vim
        ophints             ; highlight the region an operation acts on
        (popup +defaults)     ; tame sudden yet inevitable temporary windows
        ;;tabs                ; a tab bar for Emacs
        ;;treemacs            ; a project drawer, like neotree but cooler
-       unicode             ; extended unicode support for various languages
-       (vc-gutter +pretty) ; vcs diff in the fringe
+       ;;unicode             ; extended unicode support for various languages
+       (vc-gutter +pretty
+                  +diff-hl) ; vcs diff in the fringe
        vi-tilde-fringe     ; fringe tildes to mark beyond EOB
        window-select       ; visually switch windows
        ;;workspaces          ; tab emulation, persistence & separate workspaces
@@ -57,7 +57,7 @@
        :editor
        (meow +leader)
        ;;(evil +everywhere) ; come to the dark side, we have cookies
-       file-templates     ; auto-snippets for empty files
+       ;;file-templates     ; auto-snippets for empty files
        fold               ; (nigh) universal code folding
        ;;format             ; automated prettiness
        ;;god                ; run Emacs commands without modifier keys
@@ -66,16 +66,16 @@
        ;;objed              ; text object editing for the innocent
        parinfer           ; turn lisp into python, sort of
        rotate-text        ; cycle region at point between text candidates
-       snippets           ; my elves. They type so I don't have to
+       ;;snippets           ; my elves. They type so I don't have to
        word-wrap          ; soft wrapping with language-aware indent
 
        :emacs
-       (dired +icons
-              +dirvish
+       (dired +dirvish
+              +icons
               +ranger)   ; making dired pretty [functional]
        electric          ; smarter, keyword-based electric-indent
        (ibuffer +icons)  ; interactive buffer management
-       undo              ; persistent, smarter undo for your inevitable mistakes
+       (undo +tree)      ; persistent, smarter undo for your inevitable mistakes
        vc                ; version-control and Emacs, sitting in a tree
 
        :term
@@ -85,15 +85,15 @@
        ;;vterm             ; the best terminal emulation in Emacs
 
        :checkers
-       syntax            ; tasing you for every semicolon you forget
-       (spell +flyspell) ; tasing you for misspelling mispelling
-       grammar           ; tasing grammar mistake every you make
+       (syntax +childframe) ; tasing you for every semicolon you forget
+       ;;(spell +flyspell)    ; tasing you for misspelling mispelling
+       ;;grammar              ; tasing grammar mistake every you make
 
        :tools
        ;;ansible
        ;;biblio            ; Writes a PhD for you (citation needed)
        ;;collab            ; buffers with friends
-       debugger          ; FIXME stepping through code, to help you add bugs
+       (debugger +lsp)   ; FIXME stepping through code, to help you add bugs
        direnv
        ;;docker
        editorconfig      ; let someone else argue about tabs vs spaces
