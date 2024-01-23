c9403ac75772c0e58769e6779a0ada7ee75a7464
Chris <2013206+kress95@users.noreply.github.com>
2024-01-23 00:19:37 -0300

    mkIfElse

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -6,7 +6,7 @@ in
   extra = {
     attrsets = import ./attrsets.nix;
     lpm = callPackage ./lpm.nix;
-    mk-if-else = import ./mk-if-else.nix;
+    mkIfElse = callPackage import ./mk-if-else.nix;
     qt = import ./qt.nix prev;
     string = import ./string.nix;
   };
