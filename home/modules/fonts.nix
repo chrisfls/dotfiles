f0dca4456a435ccafaf9202ae1242449da1c4614
Chris <2013206+kress95@users.noreply.github.com>
2024-01-23 19:37:56 -0300

    fonts

--- home/modules/fonts.nix
+++ home/modules/fonts.nix
@@ -1,27 +1,43 @@
 { config, lib, pkgs, ... }:
-let inherit (config.modules.fonts) enable; in {
+let
+  inherit (config.presets) non-nixos;
+  inherit (config.modules.fonts) enable;
+in
+{
   options.modules.fonts.enable = lib.mkEnableOption "Enable fonts module";
 
   config = lib.mkIf enable {
-    # TODO: pacman
+    home.packages =
+      if non-nixos then
+        [ ]
+      else [
+        # good generic font
+        pkgs.noto-fonts
 
-    home.packages = [
-      # good generic font
-      pkgs.noto-fonts
+        # good emoji font
+        pkgs.noto-fonts-color-emoji
 
-      # good emoji font
-      pkgs.noto-fonts-color-emoji
+        # good heading fonts
+        pkgs.overpass
+        pkgs.montserrat
 
-      # good heading fonts
-      pkgs.overpass
-      pkgs.montserrat
+        # good terminal fonts
+        (pkgs.nerdfonts.override { fonts = [ "CascadiaCode" "JetBrainsMono" ]; })
 
-      # good terminal fonts
-      (pkgs.nerdfonts.override { fonts = [ "CascadiaCode" "JetBrainsMono" ]; })
+        # good programming fonts
+        pkgs.cascadia-code
+        pkgs.jetbrains-mono
+      ];
 
-      # good programming fonts
-      pkgs.cascadia-code
-      pkgs.jetbrains-mono
+    pacman.packages = [
+      "extra/noto-fonts"
+      "extra/noto-fonts-emoji"
+      "extra/ttf-overpass"
+      "extra/ttf-montserrat"
+      "extra/ttf-cascadia-code"
+      "extra/ttf-cascadia-code-nerd"
+      "extra/ttf-jetbrains-mono"
+      "extra/ttf-jetbrains-mono-nerd"
     ];
   };
 }
