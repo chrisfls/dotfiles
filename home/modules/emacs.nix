0327210b7efaf48a7c0a386592c87ba6269408e0
Chris <2013206+kress95@users.noreply.github.com>
2024-01-24 02:33:37 -0300

    better installer

--- home/modules/emacs.nix
+++ home/modules/emacs.nix
@@ -6,33 +6,25 @@ let
   rev = "03d692f129633e3bf0bd100d91b3ebf3f77db6d1";
   configHome = config.xdg.configHome;
   dir = "${configHome}/emacs";
+
+  doom-install =
+    pkgs.writeShellScriptBin "doom-install"
+      ''
+        mkdir -p ${dir}
+        git init ${dir}
+        git -C ${dir} remote add origin git@github.com:doomemacs/doomemacs.git
+        git -C ${dir} fetch origin ${rev}
+        git -C ${dir} reset --hard FETCH_HEAD
+        ${dir}/bin/doom build
+      '';
 in
 {
   options.modules.emacs.enable = lib.mkEnableOption "Enable emacs module";
 
   config = lib.mkIf enable {
-    home.packages = lib.mkIf (!non-nixos) [ pkgs.emacs29 pkgs.semgrep ];
-
+    home.packages = [ doom-install ] ++ (if non-nixos then [ ] else [ pkgs.emacs29 pkgs.semgrep ]);
     pacman.packages = [ "extra/emacs" ];
-
-    home.activation.doomemacs = lib.hm.dag.entryAfter [ "writeBoundary" ]
-      ''
-        if [ ! -d ${dir} ]; then
-          $DRY_RUN_CMD mkdir -p ${dir}
-          $DRY_RUN_CMD export PATH="${pkgs.emacs29}/bin:$PATH"
-          $DRY_RUN_CMD git init ${dir}
-          $DRY_RUN_CMD git -C ${dir} remote add origin git@github.com:doomemacs/doomemacs.git
-          $DRY_RUN_CMD git -C ${dir} fetch origin ${rev}
-          $DRY_RUN_CMD git -C ${dir} reset --hard FETCH_HEAD
-          $DRY_RUN_CMD ${dir}/bin/doom install
-          $DRY_RUN_CMD rm ${configHome}/doom/config.el ${configHome}/doom/init.el ${configHome}/doom/packages.el
-        fi
-      '';
-
     home.sessionPath = [ "${dir}/bin" ];
-
-    modules.copyFile = {
-      "${configHome}/doom" = "${toString ../../assets/doom}";
-    };
+    modules.copyFile."${configHome}/doom" = "${toString ../../assets/doom}";
   };
 }
