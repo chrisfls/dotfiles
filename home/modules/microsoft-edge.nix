bbf6e0e3fa9317229c3785ed36016069207da529
Chris <2013206+kress95@users.noreply.github.com>
2024-01-23 22:12:55 -0300

    edge stable

--- home/modules/microsoft-edge.nix
+++ home/modules/microsoft-edge.nix
@@ -1,32 +1,36 @@
 { config, lib, pkgs, specialArgs, ... }:
 let
-  # TODO: pacman
-
+  inherit (config.presets) non-nixos;
   inherit (config.modules.microsoft-edge) enable;
 
+  exe =
+    if non-nixos then
+      "microsoft-edge-stable"
+    else
+      "microsoft-edge";
+
   pkg =
-    if config.presets.non-nixos then
+    if non-nixos then
+      pkgs.writeShellScriptBin exe
+        ''
+          exec /usr/bin/microsoft-edge-stable --force-device-scale-factor=1.5 --enable-features=VaapiVideoDecodeLinuxGL "$@"
+        ''
+
+    else
       pkgs.microsoft-edge.overrideAttrs
         (old: {
           postFixup =
             ''
               substituteInPlace $out/bin/microsoft-edge \
-                --replace mesa dummy-mesa
-              substituteInPlace $out/bin/microsoft-edge \
-                --replace libva dummy-libva
-              substituteInPlace $out/bin/microsoft-edge \
                 --replace "--enable-features=" "--force-device-scale-factor=1.5 --enable-features=VaapiVideoDecodeLinuxGL,"
             '';
-        })
-
-    else
-      pkgs.microsoft-edge;
+        });
 in
 {
   options.modules.microsoft-edge.enable = lib.mkEnableOption "Enable microsoft-edge module";
 
   config = lib.mkIf enable {
     home.packages = [ pkg ];
-    modules.i3wm.apps."shift+b" = "microsoft-edge";
+    modules.i3wm.apps."shift+b" = exe;
   };
 }
