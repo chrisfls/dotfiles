9b45a6467d8464215b334b0f432964a5b7a1e5b6
Chris <2013206+kress95@users.noreply.github.com>
2024-01-23 22:07:54 -0300

    fmt

--- home/modules/i3wm.nix
+++ home/modules/i3wm.nix
@@ -186,11 +186,17 @@ in
   };
 
   config = lib.mkIf enable {
-    home.packages = lib.mkIf (!non-nixos) [ pkgs.i3 pkgs.cozette ];
+    home.packages =
+      if non-nixos then [ pkgs.cozette ]
+      else [ pkgs.i3 pkgs.cozette ];
 
-    pacman.packages = [ "extra/i3-wm" "chaotic-aur/cozette-otb" ];
+    pacman.packages = [ "extra/i3-wm" ];
 
     modules.xorg.window-manager = i3;
+
+    xdg.dataFile = lib.mkIf (non-nixos) {
+      "fonts/cozette".source = "${pkgs.cozette}/share/fonts";
+    };
 
     xdg.configFile."i3/config" = {
       text =
@@ -199,7 +205,7 @@ in
           # THEMING
           # ######## #### ## #
 
-          font pango:cozette 8.000000 # Cascadia Mono: 7.666666666665;
+          font pango:cozette 8 # Cascadia Mono: 7.666666666665;
 
           client.background ${black}
           client.focused ${colors.foreground} ${colors.black} ${colors.foreground} ${silver} ${gray}
