ca17bcbe653a56ee70ba368d9a856e6a024a6ef7
Chris <2013206+kress95@users.noreply.github.com>
2024-01-26 00:53:26 -0300

    uncomment unchanged bindings

--- assets/doom/modules/editor/meow/config.el
+++ assets/doom/modules/editor/meow/config.el
@@ -6,20 +6,15 @@
       doom-localleader-alt-key "C-c l")
 
 (map!
-  "M-;" 'custom/comment-or-uncomment-region
-  "C-M-\\" 'custom/indent-rigidly-right-to-tab-stop)
-
-;; TODO: bind 
-;;  avy-goto-line
-;;  avy-goto-char
+  "M-;" 'custom/comment-or-uncomment-region)
 
 (use-package! meow
   :hook (doom-after-modules-config . meow-global-mode)
   :demand t
   :config
   (meow-thing-register 'whitespace
-                     '(syntax . " ")
-                     '(syntax . " "))
+                    '(syntax . " ")
+                    '(syntax . " "))
   (setq meow-cheatsheet-layout meow-cheatsheet-layout-qwerty
         meow-cursor-type-insert '(bar . 4)
         meow-cursor-type-region-cursor '(bar . 4)
@@ -48,86 +43,96 @@
    '("<escape>" . ignore))
   (meow-leader-define-key
    ;; SPC j/k will run the original command in MOTION state.
-   '("j" . "H-j") ; j
-   '("k" . "H-k") ; k
+   '("j" . "H-j")
+   '("k" . "H-k")
    ;; Use SPC (0-9) for digit arguments.
-   '("1" . meow-digit-argument) ; 1
-   '("2" . meow-digit-argument) ; 2
-   '("3" . meow-digit-argument) ; 3
-   '("4" . meow-digit-argument) ; 4
-   '("5" . meow-digit-argument) ; 5
-   '("6" . meow-digit-argument) ; 6
-   '("7" . meow-digit-argument) ; 7
-   '("8" . meow-digit-argument) ; 8
-   '("9" . meow-digit-argument) ; 9
-   '("0" . meow-digit-argument) ; 0
-   '("?" . meow-keypad-describe-key) ; /
-   '("/" . meow-cheatsheet)) ; ?
+   '("1" . meow-digit-argument)
+   '("2" . meow-digit-argument)
+   '("3" . meow-digit-argument)
+   '("4" . meow-digit-argument)
+   '("5" . meow-digit-argument)
+   '("6" . meow-digit-argument)
+   '("7" . meow-digit-argument)
+   '("8" . meow-digit-argument)
+   '("9" . meow-digit-argument)
+   '("0" . meow-digit-argument)
+   '("/" . meow-keypad-describe-key)
+   '("?" . meow-cheatsheet))
   (meow-normal-define-key
-   '("0" . meow-expand-0) ; 0
-   '("9" . meow-expand-9) ; 9
-   '("8" . meow-expand-8) ; 8
-   '("7" . meow-expand-7) ; 7
-   '("6" . meow-expand-6) ; 6
-   '("5" . meow-expand-5) ; 5
-   '("4" . meow-expand-4) ; 4
-   '("3" . meow-expand-3) ; 3
-   '("2" . meow-expand-2) ; 2
-   '("1" . meow-expand-1) ; 1
-   '("-" . negative-argument) ; -
-   '("," . meow-reverse) ; ;
-   '("{" . meow-inner-of-thing) ; ,
-   '("}" . meow-bounds-of-thing) ; .
-   '("[" . meow-beginning-of-thing) ; [
-   '("]" . meow-end-of-thing) ; ]
-   '("a" . meow-append) ; a
-   '("o" . meow-open-below) ; A
-   '("w" . meow-back-word) ; b
-   '("W" . meow-back-symbol) ; B
-   '("c" . meow-change) ; c
+   '("0" . meow-expand-0)
+   '("9" . meow-expand-9)
+   '("8" . meow-expand-8)
+   '("7" . meow-expand-7)
+   '("6" . meow-expand-6)
+   '("5" . meow-expand-5)
+   '("4" . meow-expand-4)
+   '("3" . meow-expand-3)
+   '("2" . meow-expand-2)
+   '("1" . meow-expand-1)
+   '("-" . negative-argument)
+   '(";" . meow-reverse)
+   '("," . meow-inner-of-thing)
+   '("." . meow-bounds-of-thing)
+   '("[" . meow-beginning-of-thing)
+   '("]" . meow-end-of-thing)
+   '("a" . meow-append)
+   '("A" . meow-open-below)
+   '("b" . meow-back-word)
+   '("B" . meow-back-symbol)
+   '("c" . meow-change)
    '("x" . meow-delete) ; d
    '("X" . meow-backward-delete) ; D
-   '("e" . meow-next-word) ; e
-   '("E" . meow-next-symbol) ; E
-   '("f" . meow-find) ; f
-   '("g" . meow-cancel-selection) ; g
-   '("m" . meow-grab) ; G
-   '("h" . meow-left) ; h
-   '("H" . meow-left-expand) ; H
-   '("i" . meow-insert) ; i
-   '("O" . meow-open-above) ; I
-   '("j" . meow-next) ; j
-   '("J" . meow-next-expand) ; J
-   '("k" . meow-prev) ; k
-   '("K" . meow-prev-expand) ; K
-   '("l" . meow-right) ; l
-   '("L" . meow-right-expand) ; L
-   '("S" . meow-join) ; m
-   '("n" . meow-search) ; n
-   '("b" . meow-block) ; o
-   '("B" . meow-to-block) ; O
-   '("p" . meow-yank) ; p
-   '("q" . meow-quit) ; q
-   '(":" . meow-goto-line) ; Q
+   '("e" . meow-next-word)
+   '("E" . meow-next-symbol)
+   '("f" . meow-find)
+   '("g" . meow-cancel-selection)
+   '("G" . meow-grab)
+   '("h" . meow-left)
+   '("H" . meow-left-expand)
+   '("i" . meow-insert)
+   '("I" . meow-open-above)
+   '("j" . meow-next)
+   '("J" . meow-next-expand)
+   '("k" . meow-prev)
+   '("K" . meow-prev-expand)
+   '("l" . meow-right)
+   '("L" . meow-right-expand)
+   '("m" . meow-join)
+   '("n" . meow-search)
+   '("o" . meow-block)
+   '("O" . meow-to-block)
+   '("p" . meow-yank)
+   '("q" . meow-quit)
+   '("S" . meow-goto-line) ; Q/X
    '("P" . meow-replace) ; r
    '("r" . meow-swap-grab) ; R
    '("d" . meow-kill) ; s
-   '("t" . meow-till) ; t
-   '("u" . meow-undo) ; u
-   '("U" . meow-undo-in-selection) ; U
-   '("N" . meow-visit) ; v
-   '("v" . meow-mark-word) ; w
-   '("V" . meow-mark-symbol) ; W
+   '("t" . meow-till)
+   '("u" . meow-undo)
+   '("Z" . meow-undo-in-selection) ; U
+   '("v" . meow-visit)
+   '("w" . meow-mark-word)
+   '("W" . meow-mark-symbol)
    '("s" . meow-line) ; x
-   ;;'("G" . meow-goto-line) ; X (disabled)
-   '("y" . meow-save) ; y
-   '("Y" . meow-sync-grab) ; Y
-   '("z" . meow-pop-selection) ; z
-   '("." . repeat) ; '
-   '("<escape>" . ignore) ; <escape>
+   '("y" . meow-save)
+   '("R" . meow-sync-grab) ; Y
+   '("z" . meow-pop-selection)
+   '("'" . repeat)
+   '("<escape>" . ignore)
    ;;; new keys
-   '(";" . meow-comment)
-   '("D" . custom/delete-region)
+   '("<left>" . meow-left)
+   '("S-<left>" . meow-left-expand)
+   '("<down>" . meow-next)
+   '("S-<down>" . meow-next-expand)
+   '("<up>" . meow-prev)
+   '("S-<up>" . meow-prev-expand)
+   '("<right>" . meow-right)
+   '("S-<right>" . meow-right-expand)
+   '("/" . meow-comment)
+   '("X" . custom/delete-region)
+   '("\\l" . avy-goto-line)
+   '("\\c" . avy-goto-char)
+   '("U" . custom/meow-redo)
    ;; page movement
    '("<next>" . meow-page-down)
    '("<prior>" . meow-page-up)
@@ -141,7 +146,7 @@
    '("(" . meow-start-kmacro)
    '(")" . meow-end-kmacro)
    '("+" . meow-start-kmacro-or-insert-counter)
-   '("@" . meow-end-or-call-kmacro)))
+   '("=" . meow-end-or-call-kmacro)))
 
 (defun custom/comment-or-uncomment-region ()
   "Indent region to the right, or current line if no region is active."
@@ -149,15 +154,17 @@
   (when (meow--allow-modify-p)
     (if (use-region-p)
         (comment-or-uncomment-region (region-beginning) (region-end))
-      (comment-or-uncomment-region (line-beginning-position) (line-end-position)))))
+      (comment-or-uncomment-region (line-beginning-position) (line-end-position))))
+  (setq deactivate-mark nil))
 
-(defun custom/indent-rigidly-right-to-tab-stop ()
-  "Indent region to the right, or current line if no region is active."
-  (interactive)
-  (when (meow--allow-modify-p)
-    (if (use-region-p)
-        (indent-rigidly-right-to-tab-stop (region-beginning) (region-end))
-      (indent-rigidly-right-to-tab-stop (line-beginning-position) (line-end-position)))))
+;; (defun custom/indent-rigidly-right-to-tab-stop ()
+;;   "Indent region to the right, or current line if no region is active."
+;;   (interactive)
+;;   (when (meow--allow-modify-p)
+;;     (if (use-region-p)
+;;         (indent-rigidly-right-to-tab-stop (region-beginning) (region-end))
+;;       (indent-rigidly-right-to-tab-stop (line-beginning-position) (line-end-position)))))
+;;                (exchange-point-and-mark)
 
 (defun custom/indent-rigidly-left-to-tab-stop ()
   "Indent region to the left, or current line if no region is active."
@@ -165,7 +172,8 @@
   (when (meow--allow-modify-p)
     (if (use-region-p)
         (indent-rigidly-left-to-tab-stop (region-beginning) (region-end))
-      (indent-rigidly-left-to-tab-stop (line-beginning-position) (line-end-position)))))
+      (indent-rigidly-left-to-tab-stop (line-beginning-position) (line-end-position))))
+  (setq deactivate-mark nil))
 
 (defun custom/indent-rigidly-right ()
   "Indent region to the right, or current line if no region is active."
@@ -173,15 +181,17 @@
   (when (meow--allow-modify-p)
     (if (use-region-p)
         (indent-rigidly-right (region-beginning) (region-end))
-      (indent-rigidly-right (line-beginning-position) (line-end-position)))))
+      (indent-rigidly-right (line-beginning-position) (line-end-position))))
+  (setq deactivate-mark nil))
 
 (defun custom/indent-rigidly-left ()
   "Indent region to the left, or current line if no region is active."
   (interactive)
   (when (meow--allow-modify-p)
     (if (use-region-p)
-        (indent-rigidly-right (region-beginning) (region-end))
-      (indent-rigidly-left (line-beginning-position) (line-end-position)))))
+        (indent-rigidly-left (region-beginning) (region-end))
+      (indent-rigidly-left (line-beginning-position) (line-end-position))))
+  (setq deactivate-mark nil))
 
 (defun custom/delete-region ()
   "Deletes active region."
@@ -190,3 +200,11 @@
     (if (use-region-p)
         (call-interactively #'delete-active-region)
       (call-interactively #'delete-backward-char))))
+
+
+(defun custom/meow-redo ()
+  "Cancel current selection then redo."
+  (interactive)
+  (when (region-active-p)
+    (meow--cancel-selection))
+  (meow--execute-kbd-macro "M-_"))
