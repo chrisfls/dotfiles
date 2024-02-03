b3f0f156c623f8505d19e92b2901c6b3325f4de7
Chris <2013206+kress95@users.noreply.github.com>
2024-02-03 13:31:47 -0300

    revert autorandr module

--- home/modules/autorandr.nix
+++ home/modules/autorandr.nix
@@ -1,33 +1,11 @@
 { config, lib, pkgs, ... }:
-let
-  inherit (config.presets) archlinux;
-  inherit (config.modules.autorandr) enable;
-
-  autorandr =
-    if archlinux then "/usr/bin/autorandr"
-    else "${pkgs.autorandr}/bin/autorandr";
-in
+let inherit (config.modules.autorandr) enable; in
 {
   options.modules.autorandr.enable = lib.mkEnableOption "Enable autorandr module";
 
   config = lib.mkIf enable {
-    pacman.packages = [ "extra/autorandr" ];
-
-    modules.xorg.xsession = "${autorandr} -c";
-
-    systemd.user.services.autorandr = {
-      Unit = {
-        Description = "autorandr";
-        After = [ "graphical-session-pre.target" ];
-        PartOf = [ "graphical-session.target" ];
-      };
-
-      Service = {
-        Type = "oneshot";
-        ExecStart = "${autorandr} --change";
-      };
-
-      Install.WantedBy = [ "graphical-session.target" ];
-    };
+    programs.autorandr.enable = true;
+    services.autorandr.enable = true;
+    modules.xorg.xsession = "${pkgs.autorandr}/bin/autorandr -c";
   };
 }
