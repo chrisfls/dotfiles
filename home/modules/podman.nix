b2837ed656980bc26b66ee01de44cdb1aad80981
Chris <2013206+kress95@users.noreply.github.com>
2024-02-04 03:40:23 -0300

    note on podman setup

--- home/modules/podman.nix
+++ home/modules/podman.nix
@@ -3,6 +3,7 @@ let inherit (config.modules.podman) enable; in {
   options.modules.podman.enable = lib.mkEnableOption "Enable podman module";
 
   config = lib.mkIf enable {
+    # TODO: nix podman
     pacman.packages = [ "extra/podman" "extra/docker-compose" "extra/cni-plugins" ];
 
     home.sessionVariables.DOCKER_HOST = "unix://$XDG_RUNTIME_DIR/podman/podman.sock";
