e05b5883100414028b16c394b81b73d658c55314
Chris <2013206+kress95@users.noreply.github.com>
2024-02-14 11:20:57 -0300

    Remove windows emacs config

--- windows/unused/emacs/emacs.ps1
+++
@@ -1,7 +0,0 @@
-$Wsh = New-Object -comObject WScript.Shell
-$Link = $Wsh.CreateShortcut("$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Emacs WSL.lnk")
-$Link.TargetPath = "wslg"
-$Link.Arguments = "-d Arch --cd `"~`" -- bash --login -c `"`$HOME/.nix-profile/bin/emacs`""
-$Link.IconLocation = "$PSScriptRoot\emacs.ico"
-$Link.WorkingDirectory = "$env:HOME"
-$Link.Save()
