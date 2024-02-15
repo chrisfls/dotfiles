b0721868169427b94701f5c1f1b2a587f0be82c6
Chris <2013206+kress95@users.noreply.github.com>
2024-02-15 11:24:33 -0300

    Disable pacman overlays

--- home/modules/pacman.nix
+++ home/modules/pacman.nix
@@ -2,25 +2,25 @@
 let
   inherit (config.pacman) enable pkgs;
 
-  flatten = attrs:
-    lib.lists.concatMap
-      (value: if builtins.isAttrs value then flatten value else value)
-      (lib.attrsets.attrValues attrs);
+  # flatten = attrs:
+  #   lib.lists.concatMap
+  #     (value: if builtins.isAttrs value then flatten value else value)
+  #     (lib.attrsets.attrValues attrs);
 in
 {
   options.pacman = {
     enable = lib.mkEnableOption "Enable pacman module";
 
-    pkgs = lib.mkOption {
-      type = lib.mkOptionType {
-        name = "attrs";
-        description = "merged attribute set";
-        check = builtins.isAttrs;
-        merge = loc: lib.lists.foldl' (res: def: lib.attrsets.recursiveUpdate res def.value) { };
-        emptyValue = { value = { }; };
-      };
-      default = { };
-    };
+    # pkgs = lib.mkOption {
+    #   type = lib.mkOptionType {
+    #     name = "attrs";
+    #     description = "merged attribute set";
+    #     check = builtins.isAttrs;
+    #     merge = loc: lib.lists.foldl' (res: def: lib.attrsets.recursiveUpdate res def.value) { };
+    #     emptyValue = { value = { }; };
+    #   };
+    #   default = { };
+    # };
 
     packages = lib.mkOption {
       type = lib.types.listOf lib.types.str;
@@ -29,9 +29,8 @@ in
   };
 
   imports = [
-    ./pacman/overlay.nix
     ./pacman/pacman-switch.nix
   ];
 
-  config.pacman.packages = flatten pkgs;
+  # config.pacman.packages = flatten pkgs;
 }
