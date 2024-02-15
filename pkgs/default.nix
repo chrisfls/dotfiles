40ead31f127ca274c2b6ae3a185d72607e431b7b
Chris <2013206+kress95@users.noreply.github.com>
2024-02-15 14:58:41 -0300

    removing lpm

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,15 +1,9 @@
 final: prev:
-let
-  callPackage = path: prev.callPackage path final;
-in
-{
+let callPackage = path: prev.callPackage path final; in {
   extra = {
-    # lpm = callPackage ./lpm.nix;
     attrsets = import ./attrsets.nix;
     mkIfElse = import ./mk-if-else.nix final;
     qt = import ./qt.nix final;
     string = import ./string.nix;
   };
-
-  mopidy-youtube = callPackage ./mopidy-youtube.nix;
 }
