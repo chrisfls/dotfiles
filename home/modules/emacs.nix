c5e3eee92c5b7e469de81f24048df0d2b5d1155b
Chris <2013206+chrisfls@users.noreply.github.com>
2024-03-10 02:52:15 -0300

    Stop installing emacs with nix

--- home/modules/emacs.nix
+++ home/modules/emacs.nix
@@ -1,21 +1,6 @@
 { config, lib, pkgs, ... }:
 let
   inherit (config.modules.emacs) enable;
-
-  rev = "d657be1744a1481dc4646d0b62d5ee1d3e75d1d8";
-  configHome = config.xdg.configHome;
-  dir = "${configHome}/emacs";
-
-  doom-install =
-    pkgs.writeHostScriptBin "doom-install"
-      ''
-        mkdir -p ${dir}
-        git init ${dir}
-        git -C ${dir} remote add origin git@github.com:doomemacs/doomemacs.git
-        git -C ${dir} fetch origin ${rev}
-        git -C ${dir} reset --hard FETCH_HEAD
-        ${dir}/bin/doom build
-      '';
 in
 {
   options.modules.emacs.enable = lib.mkEnableOption "Enable emacs module";
@@ -30,7 +15,5 @@ in
     home.packages = [ doom-install ];
 
     home.sessionPath = [ "${dir}/bin" ];
-
-    modules.copyFile."${configHome}/doom" = "${toString ../../assets/doom}";
   };
 }
