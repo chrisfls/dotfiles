60274583d8c64aeea06eb97bcf10070aa3056239
Chris <2013206+chrisfls@users.noreply.github.com>
2024-03-10 01:49:29 -0300

    Normalize conf

--- assets/doom/init.el
+++ assets/doom/init.el
@@ -61,13 +61,12 @@
        ;;format             ; automated prettiness
        ;;god                ; run Emacs commands without modifier keys
        ;;lispy              ; vim for lisp, for people who don't like vim
-       ;;multiple-cursors   ; editing in many places at once
+       multiple-cursors   ; editing in many places at once
        ;;objed              ; text object editing for the innocent
        parinfer           ; turn lisp into python, sort of
        rotate-text        ; cycle region at point between text candidates
        ;;snippets           ; my elves. They type so I don't have to
        word-wrap          ; soft wrapping with language-aware indent
-       ;;(meow +leader)
 
        :emacs
        (dired +icons)    ; making dired pretty [functional]
