649f6dc2a722767a3265c2e6602ba82e70733846
Chris <2013206+chrisfls@users.noreply.github.com>
2024-03-10 21:08:59 -0300

    themed kitty tab bar

--- home/modules/kitty.nix
+++ home/modules/kitty.nix
@@ -15,19 +15,34 @@ in
     xdg.configFile."kitty/kitty.conf".text =
       with config.modules.themes.color-scheme;
       ''
+        editor micro
+        shell fish
+        
         repaint_delay 16
         input_delay 1
+        scrollback_lines 1000000
 
         font_family CaskaydiaCove NFM
         font_size 16.0
 
+        tab_bar_edge top
+        tab_bar_align left
+        tab_bar_style powerline
+        tab_powerline_style round
+        
+        tab_bar_background ${blackDim}
+        tab_bar_margin_color ${blackDim}
+
+        active_tab_foreground   ${background}
+        active_tab_background   ${foreground}
+        active_tab_font_style   bold
+        inactive_tab_foreground ${background}
+        inactive_tab_background ${blackBright}
+        inactive_tab_font_style normal
+
         remember_window_size no
         initial_window_width 80c
         initial_window_height 24c
-
-        scrollback_lines 1000000
-        editor micro
-        shell fish
 
         background ${background}
         background_opacity 0.80
@@ -36,6 +51,7 @@ in
         cursor ${foregroundBright}
         selection_foreground none
         selection_background ${foregroundDim}
+
 
         color0 ${black}
         color8 ${blackBright}
