62bdd8a5e7db0c68c094c1e61adff9ac74603e98
Chris <2013206+chrisfls@users.noreply.github.com>
2024-04-07 02:41:37 -0300

    dump down backups

--- assets/forgejo/backup.sh
+++ assets/forgejo/backup.sh
@@ -1,18 +1,5 @@
 #!/usr/bin/bash
 
-if [ -f "./data/git/forgejo.zip" ]; then
-  docker-compose run -u git server rm /data/git/forgejo.zip
-fi
-
-docker-compose run -u git server "/usr/local/bin/forgejo" dump --file "/data/git/forgejo.zip"
-
-if [ -f "./forgejo.zip" ]; then
-  rm ./forgejo.zip
-fi
-
-
-docker-compose cp server:/data/git/forgejo.zip ./forgejo.zip
-docker-compose run -u git server rm /data/git/forgejo.zip
-
-rclone copyto ./forgejo.zip cflsousa:backups/forgejo.zip -P
-rm ./forgejo.zip
+docker-compose run server tar -czvf "/data/forgejo.tar.gz" "/data/git" "/data/gitea" "/data/ssh"
+rclone copyto ./data/forgejo.tar.gz cflsousa:backups/forgejo.tar.gz -P
+docker-compose run server rm /data/forgejo.tar.gz
--- assets/forgejo/restore.sh
+++ assets/forgejo/restore.sh
@@ -1,22 +1,6 @@
 #!/usr/bin/bash
 
-if [ -f "./forgejo.zip" ]; then
-  rm forgejo.zip
-fi
-
-rclone copyto cflsousa:backups/forgejo.zip ./forgejo.zip -P
-docker-compose cp ./forgejo.zip server:/data/forgejo.zip
-docker-compose run server rm -rf /data/dump
-docker-compose run server unzip /data/forgejo.zip -d /data/dump
-
-# mv data/* /data/gitea
-docker-compose run server rm -rf /data/gitea
-docker-compose run server bash -ec 'set -e; find /data/dump/data -mindepth 1 -maxdepth 1 -exec mv -t /data/gitea/ {} +; exit'
-
-# mv repos/* /data/git/gitea-repositories/
-docker-compose run server rm -rf /data/git/gitea-repositories
-docker-compose run server bash -ec 'set -e; find /data/dump/repos -mindepth 1 -maxdepth 1 -exec mv -t /data/git/gitea-repositories/ {} +; exit'
-
-
-#/usr/local/bin/gitea -c '/data/gitea/conf/app.ini' admin regenerate hooks
-# /data/gitea/gitea.db
+rclone copyto cflsousa:backups/forgejo.tar.gz ./data/forgejo.tar.gz -P
+docker-compose run server rm -rf "/data/git" "/data/gitea" "/data/ssh"
+docker-compose run server tar -xzvf /data/forgejo.tar.gz -C /data/
+docker-compose run server rm /data/forgejo.tar.gz
