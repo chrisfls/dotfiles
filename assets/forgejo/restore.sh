c0a89b1ddb0841edcc9915cf1fb016d521312376
Chris <2013206+chrisfls@users.noreply.github.com>
2024-04-07 02:45:17 -0300

    fix restore

--- assets/forgejo/restore.sh
+++ assets/forgejo/restore.sh
@@ -2,5 +2,5 @@
 
 rclone copyto cflsousa:backups/forgejo.tar.gz ./data/forgejo.tar.gz -P
 docker-compose run server rm -rf "/data/git" "/data/gitea" "/data/ssh"
-docker-compose run server tar -xzvf /data/forgejo.tar.gz -C /data/
+docker-compose run server tar -xzvf /data/forgejo.tar.gz -C /
 docker-compose run server rm /data/forgejo.tar.gz
