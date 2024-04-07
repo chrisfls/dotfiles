4e8c3fb366e87a07947cc5c150e0f92f0c57d689
Chris <2013206+chrisfls@users.noreply.github.com>
2024-04-07 04:24:11 -0300

    move forgejo to external repo

--- assets/forgejo/backup.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-
-docker-compose run server tar -czvf "/data/forgejo.tar.gz" "/data/git" "/data/gitea" "/data/ssh"
-rclone copyto ./data/forgejo.tar.gz cflsousa:backups/forgejo.tar.gz -P
-docker-compose run server rm /data/forgejo.tar.gz
--- assets/forgejo/restore.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-rclone copyto cflsousa:backups/forgejo.tar.gz ./data/forgejo.tar.gz -P
-docker-compose run server rm -rf "/data/git" "/data/gitea" "/data/ssh"
-docker-compose run server tar -xzvf /data/forgejo.tar.gz -C /
-docker-compose run server rm /data/forgejo.tar.gz
