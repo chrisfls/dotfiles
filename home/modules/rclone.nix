52e390a7aab310085bfe0f7f853c9ba2e06c3a38
Chris <2013206+chrisfls@users.noreply.github.com>
2024-04-07 03:01:35 -0300

    use kde autostart instead of systemd

--- home/modules/rclone.nix
+++ home/modules/rclone.nix
@@ -9,24 +9,24 @@ let inherit (config.modules.rclone) enable; in {
       (pkgs.writeHostScriptBin "rclone-sync" (lib.readFile ../../assets/rclone-sync.sh))
     ];
 
-    xdg.configFile."systemd/user/rclone@.service".text =
-      ''
-        # Credits: kabili207 - https://gist.github.com/kabili207/2cd2d637e5c7617411a666d8d7e97101
+    # xdg.configFile."systemd/user/rclone@.service".text =
+    #   ''
+    #     # Credits: kabili207 - https://gist.github.com/kabili207/2cd2d637e5c7617411a666d8d7e97101
 
-        [Unit]
-        Description=rclone: Remote FUSE filesystem for cloud storage config %I
-        Documentation=man:rclone(1)
-        After=network-online.target
-        Wants=network-online.target
-        AssertPathIsDirectory="%h/mnt/%I"
+    #     [Unit]
+    #     Description=rclone: Remote FUSE filesystem for cloud storage config %I
+    #     Documentation=man:rclone(1)
+    #     After=network-online.target
+    #     Wants=network-online.target
+    #     AssertPathIsDirectory="%h/mnt/%I"
 
-        [Service]
-        Type=notify
-        ExecStart=/usr/bin/rclone mount "%I:" "%h/Desktop/cloud/%I"
-        ExecStop=fusermount -u "%h/Desktop/cloud/%I"
+    #     [Service]
+    #     Type=notify
+    #     ExecStart=/usr/bin/rclone mount "%I:" "%h/Desktop/cloud/%I"
+    #     ExecStop=fusermount -u "%h/Desktop/cloud/%I"
 
-        [Install]
-        WantedBy=default.target
-      '';
+    #     [Install]
+    #     WantedBy=default.target
+    #   '';
   };
 }
