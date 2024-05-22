b4b2612d5f76d854e25988ae25faa7f005936ee4
Chris <2013206+chrisfls@users.noreply.github.com>
2024-05-22 02:13:29 -0300

    bump configs

--- home/modules/jamesdsp.nix
+++ home/modules/jamesdsp.nix
@@ -16,9 +16,12 @@ let
       master_limrelease=100
       master_limthreshold=0
     '';
-
-  buildBase = path:
-    let graphicEQ = lib.readFile path; in
+ 
+  buildBase = name:
+    let 
+      name' = "${name} + L.txt";
+      graphicEQ = lib.readFile ../../assets/audio/equalizer/${name'};
+    in
     ''
       graphiceq_enable=true
       graphiceq_param=${graphicEQ}
@@ -60,27 +63,26 @@ in
     enable = lib.mkEnableOption "Enable jamesdsp module";
 
     presets = lib.mkOption {
-      type = lib.types.attrsOf lib.types.path;
-      default = {
-        "7Hz-Salnotes Zero" = ../../assets/audio/equalizer/${"7Hz-Salnotes Zero + L.txt"};
-        "CCA CRA" = ../../assets/audio/equalizer/${"CCA CRA + L.txt"};
-        "Etymotic ER2SE" = ../../assets/audio/equalizer/${"Etymotic ER2SE + L.txt"};
-        "Moondrop Aria SE" = ../../assets/audio/equalizer/${"Moondrop Aria SE + L.txt"};
-        "Moondrop Chu" = ../../assets/audio/equalizer/${"Moondrop Chu + L.txt"};
-        # this version might trigger the limiter too often
-        "Philips SHP9500" = ../../assets/audio/equalizer/${"Philips SHP9500 + L.txt"};
-      };
+      type = lib.types.listOf lib.types.str;
+      default = [
+        "7Hz-Salnotes Zero"
+        "CCA CRA"
+        "Etymotic ER2SE"
+        "Moondrop Aria SE"
+        "Moondrop Chu"
+        "Philips SHP9500"
+      ];
     };
   };
 
   config = lib.mkIf enable {
     pacman.packages = [ "chaotic-aur/jamesdsp-git" ];
 
-    xdg.configFile = lib.attrsets.foldlAttrs
-      (acc: name: path: acc // {
-        "jamesdsp/presets/${name} (EQ).conf".text = buildEQ path;
-        "jamesdsp/presets/${name} (CF).conf".text = buildCF path;
-        "jamesdsp/presets/${name} (IR).conf".text = buildIR path;
+    xdg.configFile = lib.lists.foldl
+      (acc: name: acc // {
+        "jamesdsp/presets/${name} (EQ).conf".text = buildEQ name;
+        "jamesdsp/presets/${name} (CF).conf".text = buildCF name;
+        "jamesdsp/presets/${name} (IR).conf".text = buildIR "${name} DF";
       })
       {
         "jamesdsp/presets/CF.conf".text =
