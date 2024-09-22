da7e3b9e225b664840b3d9ddedb97cee7f72fea6
Chris <2013206+chrisfls@users.noreply.github.com>
2024-09-22 15:52:24 -0300

    add notes about pending apps

--- home/modules/obs.nix
+++ home/modules/obs.nix
@@ -4,6 +4,7 @@ let inherit (config.modules.obs) enable; in
   options.modules.obs.enable = lib.mkEnableOption "Enable obs module";
 
   config = lib.mkIf enable {
+    # TODO: do not move to flatpak yet
     pacman.packages = [
       "chaotic-aur/obs-studio-tytan652"
       "chaotic-aur/obs-vkcapture-git"
--- home/modules/retroarch.nix
+++ home/modules/retroarch.nix
@@ -3,6 +3,7 @@ let inherit (config.modules.retroarch) enable; in
 {
   options.modules.retroarch.enable = lib.mkEnableOption "Enable retroarch module";
   config = lib.mkIf enable {
+    # TODO: move to flatpak
     pacman.packages = [
       "aur/libretro-swanstation-git"
       "chaotic-aur/libretro-mupen64plus-next-git" # extra/libretro-mupen64plus-next
