dcf9874f4d2a80f1cd80819b88bf8096d9b0e751
Chris <2013206+chrisfls@users.noreply.github.com>
2024-09-22 16:08:42 -0300

    replace edge with flatpak

--- home/modules/microsoft-edge.nix
+++ home/modules/microsoft-edge.nix
@@ -8,8 +8,9 @@ in
   options.modules.microsoft-edge.enable = lib.mkEnableOption "Enable microsoft-edge module";
 
   config = lib.mkIf enable {
-    pacman.packages = [ "chaotic-aur/${exe}-bin" ];
+    # pacman.packages = [ "chaotic-aur/${exe}-bin" ];
 
+    # TODO: check if flatpak is using this
     xdg.configFile."${exe}-flags.conf".text =
       ''
         --enable-features=WebRTCPipeWireCapturer,VaapiVideoDecodeLinuxGL
