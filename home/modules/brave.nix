17eaebd9681e62c175a39cf6f1f3d986140ec34b
Chris <2013206+chrisfls@users.noreply.github.com>
2024-09-22 16:46:04 -0300

    move brave

--- home/modules/brave.nix
+++ home/modules/brave.nix
@@ -3,8 +3,9 @@ let inherit (config.modules.brave) enable; in {
   options.modules.brave.enable = lib.mkEnableOption "Enable brave module";
 
   config = lib.mkIf enable {
-    pacman.packages = [ "chaotic-aur/brave-bin" ];
+    # pacman.packages = [ "chaotic-aur/brave-bin" ];
 
+    # TODO: check if flatpak is using this
     xdg.configFile."brave-flags.conf".text =
       ''
         --enable-features=WebRTCPipeWireCapturer,VaapiVideoDecodeLinuxGL
